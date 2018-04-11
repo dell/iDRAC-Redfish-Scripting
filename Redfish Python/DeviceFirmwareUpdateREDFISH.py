c723d9b2c3ea9877facf5cc85b01b39b8e0a65f7
texroemer <30351677+texroemer@users.noreply.github.com>
2018-04-11 13:22:43 -0500

    Add files via upload

--- Redfish Python/DeviceFirmwareUpdateREDFISH.py
+++ Redfish Python/DeviceFirmwareUpdateREDFISH.py
@@ -6,7 +6,7 @@
 # NOTE: Supported values for Install_Option are: Now, NowAndReboot and NextReboot. Make sure you pass in the exact value as stated (values are case sensitive). For NextReboot value, the update job will still get created and scheduled but will not get applied until the next server reboot executed by the user.
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 2.0
+# _version_ = 3.0
 #
 # Copyright (c) 2017, Dell, Inc.
 #
@@ -20,12 +20,12 @@
 
 import requests, json, sys, re, time, warnings
 
-
 from datetime import datetime
 
 warnings.filterwarnings("ignore")
 
 # Code to validate all correct parameters are passed in
+
 try:
     idrac_ip = sys.argv[1]
     idrac_username = sys.argv[2]
@@ -49,24 +49,25 @@ elif Install_Option == "nextreboot":
 else:
     install_option = Install_Option
 
-    # Function to check if current iDRAC version supports Redfish firmware features
+# Function to check if current iDRAC version supports Redfish firmware features
 
 def check_idrac_fw_support():
     req = requests.get('https://%s/redfish/v1/UpdateService/FirmwareInventory/' % (idrac_ip), auth=(idrac_username, idrac_password), verify=False)
     statusCode = req.status_code
     if statusCode == 400:
-        print("\n- WARNING, current server iDRAC version does not support Redfish firmware features. Refer to Dell online Redfish documentation for information on which iDRAC version support firmware features.")
+        print("\n- WARNING, current server iDRAC version does not support Redfish firmware features. Refer to Dell online Redfish documentation for information on which iDRAC version supports firmware features.")
         sys.exit()
     else:
         pass
-
+   
 # Function to download the image payload to the iDRAC
 
 def download_image_payload():
-    print("\n- WARNING, downloading DUP payload to iDRAC\n")
+    print("\n- WARNING, downloading \"%s\" image payload to iDRAC" % file_image_name)
     global Location
     global new_FW_version
     global dup_version
+    global ETag
     req = requests.get('https://%s/redfish/v1/UpdateService/FirmwareInventory/' % (idrac_ip), auth=(idrac_username, idrac_password), verify=False)
     statusCode = req.status_code
     data = req.json()
@@ -81,12 +82,9 @@ def download_image_payload():
     d = response.__dict__
     s=str(d['_content'])
     if response.status_code == 201:
-        print("\n- PASS: Command passed, 201 status code returned\n")
-        z=re.search("\"Message\":.+?,",s).group().rstrip(",")
-        z=re.sub('"',"",z)
-        print("- %s" % z)
+        print("\n- PASS: POST command passed to download image payload to iDRAC, 201 status code returned")
     else:
-        print("\n- FAIL: Post command failed to download, error is %s" % response)
+        print("\n- FAIL: POST command failed to download image payload, error is %s" % response)
         print("\nMore details on status code error: %s " % d['_content'])
         sys.exit()
     d = response.__dict__
@@ -96,15 +94,14 @@ def download_image_payload():
     zz=z.find("-")
     zz=z.find("-",zz+1)
     dup_version = z[zz+1:]
-    entry = "- FW file version is: %s" % dup_version; print(entry)
+    print("- Firmware image version of downloaded payload is: %s" % dup_version)
     Location = response.headers['Location']
-    
-    
+
 # Function to install the downloaded image payload and loop checking job status
 
 def install_image_payload():
     global job_id
-    print("\n- WARNING, installing downloaded firmware payload to device\n")
+    print("\n- WARNING, executing POST command to create firmware update job ID")
     url = 'https://%s/redfish/v1/UpdateService/Actions/Oem/DellUpdateService.Install' % (idrac_ip)
     InstallOption = install_option
     payload = "{\"SoftwareIdentityURIs\":[\"" + Location + "\"],\"InstallUpon\":\""+ InstallOption +"\"}"
@@ -113,14 +110,12 @@ def install_image_payload():
     d=str(response.__dict__)
     job_id_location = response.headers['Location']
     job_id = re.search("JID_.+",job_id_location).group()
-    print("\n- PASS, %s job ID successfully created\n" % job_id)
+    print("- PASS, %s firmware update job ID successfully created" % job_id) 
     
-
-
 # Function to check the new FW version installed
 
 def check_new_FW_version():
-    print("\n- WARNING, checking new firmware version installed for updated device\n")
+    print("\n- WARNING, checking new firmware version installed for updated device")
     req = requests.get('https://%s/redfish/v1/UpdateService/FirmwareInventory/%s' % (idrac_ip, new_FW_version), auth=(idrac_username, idrac_password), verify=False)
     statusCode = req.status_code
     data = req.json()
@@ -133,6 +128,7 @@ def check_new_FW_version():
 # Function to check the job status for host reboot needed
 
 def check_job_status_host_reboot():
+    print("\n- WARNING, script will now loop polling the job status until marked completed") 
     time.sleep(15)
     while True:
         req = requests.get('https://%s/redfish/v1/TaskService/Tasks/%s' % (idrac_ip, job_id), auth=(idrac_username, idrac_password), verify=False)
@@ -141,15 +137,15 @@ def check_job_status_host_reboot():
         message_string=data[u"Messages"]
         current_time=(datetime.now()-start_time)
         if statusCode == 202 or statusCode == 200:
-            print("\n- Query job ID command passed\n")
             time.sleep(10)
         else:
             print("Query job ID command failed, error code is: %s" % statusCode)
             sys.exit()
-        if "failed" in data[u"Messages"] or "completed with errors" in data[u"Messages"]:
+        if "failed" in data[u"Messages"] or "completed with errors" in data[u"Messages"] or "Failed" in data[u"Messages"]:
             print("- FAIL: Job failed, current message is: %s" % data[u"Messages"])
             sys.exit()
         elif data[u"TaskState"] == "Completed":
+            print("\n- PASS, job ID %s successfuly marked completed, detailed final job status results:" % data[u"Id"])
             print("\n- Job ID = "+data[u"Id"])
             print("- Name = "+data[u"Name"])
             try:
@@ -158,7 +154,12 @@ def check_job_status_host_reboot():
                 print("- Message = "+data[u"Messages"][0][u"Message"])
             print("- JobStatus = "+data[u"TaskState"])
             print("\n- %s completed in: %s" % (job_id, str(current_time)[0:7]))
-            break
+            if data[u"Name"] == "Firmware Update: iDRAC":
+                print("\n- WARNING, iDRAC update performed. Script will wait 3 minutes for iDRAC to reset and come back up before checking new firmware version")
+                time.sleep(180)
+                break
+            else:
+                break
         elif data[u"TaskState"] == "Completed with Errors" or data[u"TaskState"] == "Failed":
             print("\n- Job ID = "+data[u"Id"])
             print("- Name = "+data[u"Name"])
@@ -170,15 +171,13 @@ def check_job_status_host_reboot():
             print("\n- %s completed in: %s" % (job_id, str(current_time)[0:7]))
             sys.exit()
         else:
-            print("- Job not marked completed, current status is: %s" % data[u"TaskState"])
-            print("- Message: %s\n" % message_string[0][u"Message"])
-            print("- Current job execution time is: %s\n" % str(current_time)[0:7])
+            print("- Message: %s" % message_string[0][u"Message"])
             time.sleep(1)
             continue
 
-def check_job_status():
-    # Loop get commnad to check the job status of completed, completed with errors or failed
-    #start_time=datetime.now()
+# Function to check job status for next manual reboot
+
+def check_job_status(): 
     while True:
         req = requests.get('https://%s/redfish/v1/TaskService/Tasks/%s' % (idrac_ip, job_id), auth=(idrac_username, idrac_password), verify=False)
         statusCode = req.status_code
@@ -186,7 +185,7 @@ def check_job_status():
         message_string=data[u"Messages"]
         current_time=(datetime.now()-start_time)
         if statusCode == 202 or statusCode == 200:
-            print("\n- Query job ID command passed\n")
+            #print("\n- Query job ID command passed\n")
             time.sleep(10)
         else:
             print("Query job ID command failed, error code is: %s" % statusCode)
@@ -227,15 +226,16 @@ def check_job_status():
             continue
 
 
-# Run code here
+# Run code
 
-check_idrac_fw_support()
-download_image_payload()
-install_image_payload()
-if install_option == "NowAndReboot" or install_option == "Now":
-    check_job_status_host_reboot()
-    check_new_FW_version()
-else:
-    check_job_status()
+if __name__ == "__main__":
+    check_idrac_fw_support()
+    download_image_payload()
+    install_image_payload()
+    if install_option == "NowAndReboot" or install_option == "Now":
+        check_job_status_host_reboot()
+        check_new_FW_version()
+    else:
+        check_job_status()
 
 
