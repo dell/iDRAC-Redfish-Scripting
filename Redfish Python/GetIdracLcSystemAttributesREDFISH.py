4f67736f401098f06c294f7b5d9dea7ab7b16274
texroemer <30351677+texroemer@users.noreply.github.com>
2018-09-06 14:04:50 -0500

    Add files via upload

--- Redfish Python/GetIdracLcSystemAttributesREDFISH.py
+++ Redfish Python/GetIdracLcSystemAttributesREDFISH.py
@@ -6,7 +6,7 @@
 # NOTE: Possible supported values for attribute_group parameter are: idrac, lc and system.
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 2.0
+# _version_ = 4.0
 #
 # Copyright (c) 2017, Dell, Inc.
 #
@@ -18,39 +18,46 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
 
-import requests, json, sys, re, time, warnings
+import requests, json, sys, re, time, warnings, argparse
 
 from datetime import datetime
 
 warnings.filterwarnings("ignore")
 
-try:
-    idrac_ip = sys.argv[1]
-    idrac_username = sys.argv[2]
-    idrac_password = sys.argv[3]
-    attribute_group = sys.argv[4]
+parser=argparse.ArgumentParser(description="Python script using Redfish API to get either iDRAC, lifecycle controller or system attributes")
+parser.add_argument('-ip',help='iDRAC IP address', required=True)
+parser.add_argument('-u', help='iDRAC username', required=True)
+parser.add_argument('-p', help='iDRAC password', required=True)
+parser.add_argument('script_examples',action="store_true",help='GetIdracLcSystemAttributesREDFISH.py -ip 192.168.0.120 -u root -p calvin -g idrac, this example wil get all iDRAC attributes and echo them to the screen along with copy output to a file.') 
+parser.add_argument('-g', help='Get attributes, pass in the group name of the attributes you want to get. Supported values are \"idrac\", \"lc\" and \"system\"', required=True)
 
-except:
-    print("- FAIL: You must pass in script name along with iDRAC IP / iDRAC username / iDRAC password / attribute group. Example: \"script_name.py 192.168.0.120 root calvin idrac\"")
-    sys.exit()
+args=vars(parser.parse_args())
 
-### Function to get attributes and current value
+idrac_ip=args["ip"]
+idrac_username=args["u"]
+idrac_password=args["p"]
+
+def check_supported_idrac_version():
+    response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code != 200:
+        print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        print("\nNote: If using iDRAC 7/8, this script is not supported. Use Server Configuration Profile feature instead with Redfish to get iDRAC / System and Lifecycle Controller attributes") 
+        sys.exit()
+    else:
+        pass
 
 def get_attribute_group():
     global current_value
-    if attribute_group == "idrac":
+    if args["g"] == "idrac":
         response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    elif attribute_group == "lc":
+    elif args["g"] == "lc":
         response = requests.get('https://%s/redfish/v1/Managers/LifecycleController.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    elif attribute_group == "system":
+    elif args["g"] == "system":
         response = requests.get('https://%s/redfish/v1/Managers/System.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
     data = response.json()
-    try:
-        attributes_dict=data[u'Attributes']
-    except:
-        print "\n- WARNING, current iDRAC version on server does not support this feature using Redfish API"
-        sys.exit()
-    print("\n- %s Attribute Names and Values:\n" % attribute_group.upper())
+    attributes_dict=data[u'Attributes']
+    print("\n- %s Attribute Names and Values:\n" % args["g"].upper())
     f = open("attributes.txt","w")
     for i in attributes_dict:
         z="Name: %s, Value: %s" % (i, attributes_dict[i])
@@ -60,9 +67,8 @@ def get_attribute_group():
     print("\n- WARNING, Attribute enumeration also copied to \"attributes.txt\" file")
     
 
-### Run code
-
 if __name__ == "__main__":
+    check_supported_idrac_version()
     get_attribute_group()
 
 
--- Redfish Python/SetIdracLcSystemAttributesREDFISH.py
+++ Redfish Python/SetIdracLcSystemAttributesREDFISH.py
@@ -3,12 +3,10 @@
 #
 # NOTE: Recommended to run script GetIdracLcSystemAttributesREDFISH first to return attributes with current values. 
 #
-# NOTE: Before executing the script, edit "file_attributes_dict" dictionary below first. You want to pass in the attributes you want to change along with the new value. The script only supports setting one group of attributes (Example: you can only pass in iDRAC attributes in the dictionary. When you execute the script, you want to pass in "idrac" for the attribute_group parameter).
-#
 # NOTE: Possible supported values for attribute_group parameter are: idrac, lc and system.
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 2.0
+# _version_ = 3.0
 #
 # Copyright (c) 2017, Dell, Inc.
 #
@@ -21,91 +19,94 @@
 #
 
 
-import requests, json, sys, re, time, warnings
+import requests, json, sys, re, time, warnings, argparse
 
 from datetime import datetime
 
 warnings.filterwarnings("ignore")
 
-try:
-    idrac_ip = sys.argv[1]
-    idrac_username = sys.argv[2]
-    idrac_password = sys.argv[3]
-    attribute_group = sys.argv[4]
-
-except:
-    print("- FAIL: You must pass in script name along with iDRAC IP / iDRAC username / iDRAC password / attribute group. Example: \"script_name.py 192.168.0.120 root calvin idrac\"")
-    sys.exit()
-
-file_attributes_dict = {"Telnet.1.Enable":"Disabled"}
+parser=argparse.ArgumentParser(description="Python script using Redfish API to set either iDRAC, lifecycle controller or system attributes")
+parser.add_argument('-ip',help='iDRAC IP address', required=True)
+parser.add_argument('-u', help='iDRAC username', required=True)
+parser.add_argument('-p', help='iDRAC password', required=True)
+parser.add_argument('script_examples',action="store_true",help='SetIdracLcSystemAttributesREDFISH.py -ip 192.168.0.120 -u root -p calvin -s idrac -an Time.1.Timezone,Telnet.1.Enable,RemoteHosts.1.SMTPServerIPAddress -av CST6CDT,enabled,test.labs.net, this example is setting 3 iDRAC attributes.') 
+parser.add_argument('-s', help='Set attributes, pass in the group name of the attributes you want to configure. Supported values are \"idrac\", \"lc\" and \"system\"', required=True)
+parser.add_argument('-an', help='Pass in the attribute name you want to configure. If you want to configure multiple attribute names, make sure to use a comma separator between each attribute name. Note: Make sure you are passing in the correct attributes which match the value you are passing in for argument -s. Note: Attribute names are case sensitive, make sure to pass in the exact syntax of the attribute name', required=True)
+parser.add_argument('-av', help='Pass in the attribute value you want to set the attribute to. If you want to configure multiple attribute values, make sure to use a comma separator between each attribute value. Note: Attribute values are case sensitive, make sure to pass in the exact syntax of the attribute value', required=True)
 
 
-### Function to set attributes new values from file_attributes_dict dictionary
+
+args=vars(parser.parse_args())
+
+idrac_ip=args["ip"]
+idrac_username=args["u"]
+idrac_password=args["p"]
+
+
+def check_supported_idrac_version():
+    response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code != 200:
+        print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        print("\nNote: If using iDRAC 7/8, this script is not supported. Use Server Configuration Profile feature instead with Redfish to set iDRAC / System and Lifecycle Controller attributes") 
+        sys.exit()
+    else:
+        pass
+
 
 def set_attributes():
-    if attribute_group == "idrac":
-        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    elif attribute_group == "lc":
-        response = requests.get('https://%s/redfish/v1/Managers/LifecycleController.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    elif attribute_group == "system":
-        response = requests.get('https://%s/redfish/v1/Managers/System.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    data = response.json()
-    try:
-        attributes_dict=data[u'Attributes']
-    except:
-        print "\n- WARNING, current iDRAC version on server does not support this feature using Redfish API"
-        sys.exit()
-    print("- WARNING, changing %s attributes:\n" % attribute_group.upper())
-    for i in file_attributes_dict:
-        print(" Name:  %s, setting new value to: %s" % (i, file_attributes_dict[i]))
-    payload = {"Attributes":file_attributes_dict}
-    headers = {'content-type': 'application/json'}
-    if attribute_group == "idrac":
+    global url
+    global payload
+    if args["s"] == "idrac":
         url = 'https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Attributes' % idrac_ip
-    elif attribute_group == "lc":
+    elif args["s"] == "lc":
         url = 'https://%s/redfish/v1/Managers/LifecycleController.Embedded.1/Attributes' % idrac_ip
-    elif attribute_group == "system":
+    elif args["s"] == "system":
         url = 'https://%s/redfish/v1/Managers/System.Embedded.1/Attributes' % idrac_ip
-    
-    response = requests.patch(url, data=json.dumps(payload), headers=headers, verify=False,auth=(idrac_username, idrac_password))
-    
-    statusCode = response.status_code
-    
-    if statusCode == 200:
-        print("\n- PASS, Command passed to successfully set \"%s\" attribute(s), status code %s returned\n" % (attribute_group.upper(),statusCode))
     else:
-        print("\n- FAIL, Command failed to set %s attributes(s), status code is: %s\n" % (attribute_group.upper(),statusCode))
+        print("\n- FAIL, invalid value entered for -s argument")
+        sys.exit()
+    response = requests.get('%s' % (url),verify=False,auth=(idrac_username, idrac_password))
+    data = response.json()
+    payload = {"Attributes":{}}
+    attribute_names = args["an"].split(",")
+    attribute_values = args["av"].split(",")
+    for i,ii in zip(attribute_names, attribute_values):
+        payload["Attributes"][i] = ii
+    print("\n- WARNING, changing \"%s\" attributes -\n" % args["s"].upper())
+    for i in payload["Attributes"].items():
+        print(" Attribute Name: %s, setting new value to: %s" % (i[0], i[1]))
+    headers = {'content-type': 'application/json'}
+    response = requests.patch(url, data=json.dumps(payload), headers=headers, verify=False,auth=(idrac_username, idrac_password))
+    statusCode = response.status_code
+    if statusCode == 200:
+        print("\n- PASS, Command passed to successfully set \"%s\" attribute(s), status code %s returned\n" % (args["s"].upper(),statusCode))
+    else:
+        print("\n- FAIL, Command failed to set %s attributes(s), status code is: %s\n" % (args["s"].upper(),statusCode))
         print("Extended Info Message: {0}".format(response.json()))
         sys.exit()
     
-
-
-### Function to verify new current values
     
 def get_new_attribute_values():
-    if attribute_group == "idrac":
-        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    elif attribute_group == "lc":
-        response = requests.get('https://%s/redfish/v1/Managers/LifecycleController.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    elif attribute_group == "system":
-        response = requests.get('https://%s/redfish/v1/Managers/System.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    print("- WARNING, checking new attribute values - \n")
+    response = requests.get('%s' % (url),verify=False,auth=(idrac_username, idrac_password))
     data = response.json()
     new_attributes_dict=data[u'Attributes']
-    for i in file_attributes_dict.items():
-        for ii in new_attributes_dict.items():
+    new_attribute_values = {"Attributes":{}}
+    for i in new_attributes_dict.items():
+        for ii in payload["Attributes"].items():
             if i[0] == ii[0]:
-                if i[1] == ii[1]:
+                if i[1].lower() == ii[1].lower():
                     print("- PASS, Attribute %s successfully set to %s" % (i[0],i[1]))
                 else:
                     print("- FAIL, Attribute %s not set to %s" % (i[0],i[1]))
         
-    
 
-### Run code
 
 if __name__ == "__main__":
-    set_attributes()
-    get_new_attribute_values()
+    if args["s"] and args["an"] and args["av"]:
+        set_attributes()
+        get_new_attribute_values()
     
 
 
