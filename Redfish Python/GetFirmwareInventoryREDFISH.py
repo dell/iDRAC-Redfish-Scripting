ca0fe723384e285d4eb28c2fb3087781388fc44e
texroemer <30351677+texroemer@users.noreply.github.com>
2019-03-06 13:34:32 -0600

    Add files via upload
    Updated script to make one GET call using OData feature $expand

--- Redfish Python/GetFirmwareInventoryREDFISH.py
+++ Redfish Python/GetFirmwareInventoryREDFISH.py
@@ -1,12 +1,12 @@
 #
-# GetFirmwareInventoryREDFISH. Python script using Redfish API to get current firmware version for all devices iDRAC supports for updates.
+# GetFirmwareInventoryREDFISH. Python script using Redfish API DMTF method to get current firmware version for all devices iDRAC supports for updates.
 #
 # NOTE: Recommended to run this script first to get current FW versions of devices before executing DeviceFirmwareUpdateREDFISH script.
 #
 # 
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 3.0
+# _version_ = 4.0
 #
 # Copyright (c) 2018, Dell, Inc.
 #
@@ -18,21 +18,27 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
 
-import requests, json, sys, re, time, warnings, os
+import requests, json, sys, re, time, warnings, os, argparse
+
+import requests, json, sys, re, time, os, warnings, argparse
 
 from datetime import datetime
 
 warnings.filterwarnings("ignore")
 
-try:
-    idrac_ip = sys.argv[1]
-    idrac_username = sys.argv[2]
-    idrac_password = sys.argv[3]
-except:
-    print("\n- FAIL, you must pass in script name along with iDRAC IP / iDRAC username / iDRAC password. Example: \"script_name.py 192.168.0.120 root calvin\"")
-    sys.exit()
+parser=argparse.ArgumentParser(description="Python script using Redfish API DMTF method to get current firmware version for all devices iDRAC supports for updates. This script is excuting one GET command using OData feature $expand ")
+parser.add_argument('-ip',help='iDRAC IP address', required=True)
+parser.add_argument('-u', help='iDRAC username', required=True)
+parser.add_argument('-p', help='iDRAC password', required=True)
 
-# Function to check if current iDRAC version supports Redfish firmware features
+args=vars(parser.parse_args())
+
+idrac_ip=args["ip"]
+idrac_username=args["u"]
+idrac_password=args["p"]
+
+
+
 
 def check_idrac_fw_support():
     req = requests.get('https://%s/redfish/v1/UpdateService/FirmwareInventory/' % (idrac_ip), auth=(idrac_username, idrac_password), verify=False)
@@ -44,52 +50,48 @@ def check_idrac_fw_support():
         pass
     
 
-
-# Function to get FW inventory
-
 def get_FW_inventory():
-        print("\n- WARNING, get current firmware version(s) for all devices in the system iDRAC supports\n")
-        time.sleep(3)
-        try:
-            os.remove("fw_inventory.txt")
-        except:
-            pass
-        f=open("fw_inventory.txt","a")
-        d=datetime.now()
-        current_date_time="- Data collection timestamp: %s-%s-%s  %s:%s:%s\n" % (d.month,d.day,d.year, d.hour,d.minute,d.second)
-        f.writelines(current_date_time)
-        f.writelines("\n\n")
-        req = requests.get('https://%s/redfish/v1/UpdateService/FirmwareInventory/' % (idrac_ip), auth=(idrac_username, idrac_password), verify=False)
-        statusCode = req.status_code
-        data = req.json()
-        number_of_devices=len(data[u'Members'])
-        count = 0
-        installed_devices=[]
-        while count != len(data[u'Members']):
-            a=data[u'Members'][count][u'@odata.id']
-            a=a.replace("/redfish/v1/UpdateService/FirmwareInventory/","")
-            if "Installed" in a:
-                installed_devices.append(a)
-            count +=1
-        for i in installed_devices:
-            req = requests.get('https://%s/redfish/v1/UpdateService/FirmwareInventory/%s' % (idrac_ip, i), auth=(idrac_username, idrac_password), verify=False)
-            statusCode = req.status_code
-            data = req.json()
-            for i in data.items():
-                if i[0] == u'Description':
-                    entry = "%s: %s\n" % (i[0], i[1])
-                    f.writelines("%s\n" % entry)
-                    print(entry)
-                    f.writelines("\n")
-    
-                else:
-                    entry = "%s: %s" % (i[0], i[1])
-                    print(entry)
-                    f.writelines("%s\n" % entry)
+    print("\n- WARNING, get current firmware version(s) for all devices in the system iDRAC supports\n")
+    time.sleep(3)
+    try:
+        os.remove("fw_inventory.txt")
+    except:
+        pass
+    f=open("fw_inventory.txt","a")
+    d=datetime.now()
+    current_date_time="- Data collection timestamp: %s-%s-%s  %s:%s:%s\n" % (d.month,d.day,d.year, d.hour,d.minute,d.second)
+    f.writelines(current_date_time)
+    f.writelines("\n\n")
+    req = requests.get('https://%s/redfish/v1/UpdateService/FirmwareInventory?$expand=*($levels=1)' % (idrac_ip), auth=(idrac_username, idrac_password), verify=False)
+    statusCode = req.status_code
+    data = req.json()
+    for i in data[u'Members']:
+        for ii in i.items():
+            if ii[0] == u'@odata.type':
+                message = "\n%s: %s" % (ii[0], ii[1])
+                f.writelines(message)
+                print(message)
+                message = "\n"
+                f.writelines(message)
+            elif ii[0] == "Oem":
+                for iii in ii[1][u'Dell'][u'DellSoftwareInventory'].items():
+                    message = "%s: %s" % (iii[0], iii[1])
+                    f.writelines(message)
+                    print(message)
+                    message = "\n"
+                    f.writelines(message)
 
-        print("\n- WARNING, software inventory also captured in \"fw_inventory.txt\" file")
-        f.close()
-        sys.exit()
+            else:
+                message = "%s: %s" % (ii[0], ii[1])
+                f.writelines(message)
+                print(message)
+                message = "\n"
+                f.writelines(message)
+
+    print("\n- Firmware inventory output is also captured in \"fw_inventory.txt\" file")
+    f.close()
+        
+        
 
 
 if __name__ == "__main__":
