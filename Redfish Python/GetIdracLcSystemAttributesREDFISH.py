9acf81e7894760f26cecd81d50f3cb1a9a35f6de
texroemer <30351677+texroemer@users.noreply.github.com>
2019-04-01 14:13:03 -0500

    Updated scripts with attribute registry support

--- Redfish Python/GetIdracLcSystemAttributesREDFISH.py
+++ Redfish Python/GetIdracLcSystemAttributesREDFISH.py
@@ -6,7 +6,7 @@
 # NOTE: Possible supported values for attribute_group parameter are: idrac, lc and system.
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 4.0
+# _version_ = 5.0
 #
 # Copyright (c) 2017, Dell, Inc.
 #
@@ -28,8 +28,11 @@ parser=argparse.ArgumentParser(description="Python script using Redfish API to g
 parser.add_argument('-ip',help='iDRAC IP address', required=True)
 parser.add_argument('-u', help='iDRAC username', required=True)
 parser.add_argument('-p', help='iDRAC password', required=True)
-parser.add_argument('script_examples',action="store_true",help='GetIdracLcSystemAttributesREDFISH.py -ip 192.168.0.120 -u root -p calvin -g idrac, this example wil get all iDRAC attributes and echo them to the screen along with copy output to a file.') 
-parser.add_argument('-g', help='Get attributes, pass in the group name of the attributes you want to get. Supported values are \"idrac\", \"lc\" and \"system\"', required=True)
+parser.add_argument('script_examples',action="store_true",help='GetIdracLcSystemAttributesREDFISH.py -ip 192.168.0.120 -u root -p calvin -g idrac, this example wil get all iDRAC attributes and echo them to the screen along with copy output to a file. GetIdracLcSystemAttributesREDFISH.py -ip 192.168.0.120 -u root -p calvin -g idrac -an LDAPRoleGroup.1.Privilege, this example will only return current value for attribute LDAPRoleGroup.1.Privilege. GetIdracLcSystemAttributesREDFISH.py -ip 192.168.0.120 -u root -p calvin -g idrac -ar y, this example will return the attribute registry for iDRAC, LC and System attributes.') 
+parser.add_argument('-g', help='Get attributes, pass in the group name of the attributes you want to get. Supported values are \"idrac\", \"lc\" and \"system\"', required=False)
+parser.add_argument('-an', help='Get specific attribute value, pass in the attribute name', required=False)
+parser.add_argument('-ar', help='Pass in \"y\" to get the attribute registry for all iDRAC, System and LC attributes. This option is helpful for viewing attributes to see if they are read only or read write, supported possible values.', required=False)
+parser.add_argument('-s', help='Get attribute registry information for a specific attribute, pass in the attribute name', required=False)
 
 args=vars(parser.parse_args())
 
@@ -46,6 +49,43 @@ def check_supported_idrac_version():
         sys.exit()
     else:
         pass
+
+def get_attribute_registry():
+    try:
+        os.remove("idrac_attribute_registry.txt")
+    except:
+        pass
+    f=open("idrac_attribute_registry.txt","a")
+    response = requests.get('https://%s/redfish/v1/Registries/ManagerAttributeRegistry/ManagerAttributeRegistry.v1_0_0.json' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    data = response.json()
+    for i in data[u'RegistryEntries']['Attributes']:
+        for ii in i.items():
+            message = "%s: %s" % (ii[0], ii[1])
+            f.writelines(message)
+            print(message)
+            message = "\n"
+            f.writelines(message)
+        message = "\n"
+        print(message)
+        f.writelines(message)
+    print("\n- Attribute registry is also captured in \"idrac_attribute_registry.txt\" file")
+    f.close()
+
+
+def attribute_registry_get_specific_attribute():
+    print("\n- WARNING, searching attribute registry for attribute \"%s\"" % args["s"])
+    response = requests.get('https://%s/redfish/v1/Registries/ManagerAttributeRegistry/ManagerAttributeRegistry.v1_0_0.json' % idrac_ip,verify=False,auth=(idrac_username,idrac_password))
+    data = response.json()
+    found = ""
+    for i in data[u'RegistryEntries']['Attributes']:
+        if args["s"] in i.values():
+            print("\n- Attribute Registry information for attribute \"%s\" -\n" % args["s"])
+            found = "yes"
+            for ii in i.items():
+                print("%s: %s" % (ii[0],ii[1]))
+    if found != "yes":
+        print("\n- FAIL, unable to locate attribute \"%s\" in the registry. Make sure you typed the attribute name correct since its case sensitive" % args["s"])
+        
 
 def get_attribute_group():
     global current_value
@@ -65,10 +105,43 @@ def get_attribute_group():
         f.writelines("%s\n" % z)
     f.close()
     print("\n- WARNING, Attribute enumeration also copied to \"attributes.txt\" file")
+
+
+    
+def get_specific_attribute():
+    global current_value
+    if args["g"] == "idrac":
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    elif args["g"] == "lc":
+        response = requests.get('https://%s/redfish/v1/Managers/LifecycleController.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    elif args["g"] == "system":
+        response = requests.get('https://%s/redfish/v1/Managers/System.Embedded.1/Attributes' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    data = response.json()
+    attributes_dict=data[u'Attributes']
+    for i in attributes_dict:
+        if i == args["an"]:
+            print("\nAttribute Name: %s, Current Value: %s" % (i, attributes_dict[i]))
+            sys.exit()
+    print("\n- FAIL, unable to locate attribute \"%s\". Either current iDRAC version installed doesn\'t support this attribute or iDRAC missing required license" % args["an"])
+
     
 
+
+    
 if __name__ == "__main__":
     check_supported_idrac_version()
-    get_attribute_group()
+    if args["ar"]:
+        get_attribute_registry()
+    elif args["s"]:
+        attribute_registry_get_specific_attribute()
+    elif args["g"] and args["an"]:
+        get_specific_attribute()
+    elif args["g"]:
+        get_attribute_group()
+    else:
+        print("- FAIL, either missing parameter(s) or invalid paramter value(s) passed in. Refer to help text if needed for supported parameters and values along with script examples")
+        
+        
+        
 
 
--- Redfish Python/SetIdracLcSystemAttributesREDFISH.py
+++ Redfish Python/SetIdracLcSystemAttributesREDFISH.py
@@ -6,7 +6,7 @@
 # NOTE: Possible supported values for attribute_group parameter are: idrac, lc and system.
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 3.0
+# _version_ = 4.0
 #
 # Copyright (c) 2017, Dell, Inc.
 #
@@ -30,10 +30,11 @@ parser.add_argument('-ip',help='iDRAC IP address', required=True)
 parser.add_argument('-u', help='iDRAC username', required=True)
 parser.add_argument('-p', help='iDRAC password', required=True)
 parser.add_argument('script_examples',action="store_true",help='SetIdracLcSystemAttributesREDFISH.py -ip 192.168.0.120 -u root -p calvin -s idrac -an Time.1.Timezone,Telnet.1.Enable,RemoteHosts.1.SMTPServerIPAddress -av CST6CDT,enabled,test.labs.net, this example is setting 3 iDRAC attributes.') 
-parser.add_argument('-s', help='Set attributes, pass in the group name of the attributes you want to configure. Supported values are \"idrac\", \"lc\" and \"system\"', required=True)
-parser.add_argument('-an', help='Pass in the attribute name you want to configure. If you want to configure multiple attribute names, make sure to use a comma separator between each attribute name. Note: Make sure you are passing in the correct attributes which match the value you are passing in for argument -s. Note: Attribute names are case sensitive, make sure to pass in the exact syntax of the attribute name', required=True)
-parser.add_argument('-av', help='Pass in the attribute value you want to set the attribute to. If you want to configure multiple attribute values, make sure to use a comma separator between each attribute value. Note: Attribute values are case sensitive, make sure to pass in the exact syntax of the attribute value', required=True)
-
+parser.add_argument('-s', help='Set attributes, pass in the group name of the attributes you want to configure. Supported values are \"idrac\", \"lc\" and \"system\"', required=False)
+parser.add_argument('-an', help='Pass in the attribute name you want to configure. If you want to configure multiple attribute names, make sure to use a comma separator between each attribute name. Note: Make sure you are passing in the correct attributes which match the value you are passing in for argument -s. Note: Attribute names are case sensitive, make sure to pass in the exact syntax of the attribute name', required=False)
+parser.add_argument('-av', help='Pass in the attribute value you want to set the attribute to. If you want to configure multiple attribute values, make sure to use a comma separator between each attribute value. Note: Attribute values are case sensitive, make sure to pass in the exact syntax of the attribute value', required=False)
+parser.add_argument('-ar', help='Pass in \"y\" to get the attribute registry for all iDRAC, System and LC attributes. This option is helpful for viewing attributes to see if they are read only or read write, supported possible values.', required=False)
+parser.add_argument('-ars', help='Get attribute registry information for a specific attribute, pass in the attribute name', required=False)
 
 
 args=vars(parser.parse_args())
@@ -54,6 +55,44 @@ def check_supported_idrac_version():
         pass
 
 
+def get_attribute_registry():
+    try:
+        os.remove("idrac_attribute_registry.txt")
+    except:
+        pass
+    f=open("idrac_attribute_registry.txt","a")
+    response = requests.get('https://%s/redfish/v1/Registries/ManagerAttributeRegistry/ManagerAttributeRegistry.v1_0_0.json' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    data = response.json()
+    for i in data[u'RegistryEntries']['Attributes']:
+        for ii in i.items():
+            message = "%s: %s" % (ii[0], ii[1])
+            f.writelines(message)
+            print(message)
+            message = "\n"
+            f.writelines(message)
+        message = "\n"
+        print(message)
+        f.writelines(message)
+    print("\n- Attribute registry is also captured in \"idrac_attribute_registry.txt\" file")
+    f.close()
+
+
+def attribute_registry_get_specific_attribute():
+    print("\n- WARNING, searching attribute registry for attribute \"%s\"" % args["ars"])
+    response = requests.get('https://%s/redfish/v1/Registries/ManagerAttributeRegistry/ManagerAttributeRegistry.v1_0_0.json' % idrac_ip,verify=False,auth=(idrac_username,idrac_password))
+    data = response.json()
+    found = ""
+    for i in data[u'RegistryEntries']['Attributes']:
+        if args["ars"] in i.values():
+            print("\n- Attribute Registry information for attribute \"%s\" -\n" % args["ars"])
+            found = "yes"
+            for ii in i.items():
+                print("%s: %s" % (ii[0],ii[1]))
+    if found != "yes":
+        print("\n- FAIL, unable to locate attribute \"%s\" in the registry. Make sure you typed the attribute name correct since its case sensitive" % args["ars"])
+
+        
+
 def set_attributes():
     global url
     global payload
@@ -68,12 +107,23 @@ def set_attributes():
         sys.exit()
     response = requests.get('%s' % (url),verify=False,auth=(idrac_username, idrac_password))
     data = response.json()
+    
     payload = {"Attributes":{}}
     attribute_names = args["an"].split(",")
     attribute_values = args["av"].split(",")
     for i,ii in zip(attribute_names, attribute_values):
         payload["Attributes"][i] = ii
     print("\n- WARNING, changing \"%s\" attributes -\n" % args["s"].upper())
+    for i in payload["Attributes"].items():
+        response = requests.get('https://%s/redfish/v1/Registries/ManagerAttributeRegistry/ManagerAttributeRegistry.v1_0_0.json' % idrac_ip,verify=False,auth=(idrac_username,idrac_password))
+        data = response.json()
+        for ii in data[u'RegistryEntries']['Attributes']:
+            if i[0] in ii.values():
+                for iii in ii.items():
+                    if iii[0] == "Type":
+                        if iii[1] == "Integer":
+                            payload["Attributes"][i[0]] = int(i[1])
+    
     for i in payload["Attributes"].items():
         print(" Attribute Name: %s, setting new value to: %s" % (i[0], i[1]))
     headers = {'content-type': 'application/json'}
@@ -88,25 +138,30 @@ def set_attributes():
     
     
 def get_new_attribute_values():
-    print("- WARNING, checking new attribute values - \n")
+    print("- WARNING, getting new attribute values - \n")
     response = requests.get('%s' % (url),verify=False,auth=(idrac_username, idrac_password))
     data = response.json()
-    new_attributes_dict=data[u'Attributes']
-    new_attribute_values = {"Attributes":{}}
-    for i in new_attributes_dict.items():
-        for ii in payload["Attributes"].items():
-            if i[0] == ii[0]:
-                if i[1].lower() == ii[1].lower():
-                    print("- PASS, Attribute %s successfully set to %s" % (i[0],i[1]))
-                else:
-                    print("- FAIL, Attribute %s not set to %s" % (i[0],i[1]))
+    attributes_dict=data[u'Attributes']
+    for i in payload["Attributes"].items():
+        for ii in attributes_dict:
+            if ii == i[0]:
+                print("Attribute Name: %s, Attribute Value: %s" % (i[0], attributes_dict[ii]))
+            
+
         
 
 
 if __name__ == "__main__":
-    if args["s"] and args["an"] and args["av"]:
+    check_supported_idrac_version()
+    if args["ar"]:
+        get_attribute_registry()
+    elif args["ars"]:
+        attribute_registry_get_specific_attribute()
+    elif args["s"] and args["an"] and args["av"]:
         set_attributes()
         get_new_attribute_values()
+    else:
+        print("- FAIL, either missing parameter(s) or invalid paramter value(s) passed in. Refer to help text if needed for supported parameters and values along with script examples")
     
 
 
