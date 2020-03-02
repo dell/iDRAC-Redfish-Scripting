86e68df775c0a03be57e2c6c2cd34161e839c011
texroemer <30351677+texroemer@users.noreply.github.com>
2020-03-02 10:39:36 -0600

    Updated to python 3, argparse support

--- Redfish Python/ChangeIdracUserPasswordREDFISH.py
+++ Redfish Python/ChangeIdracUserPasswordREDFISH.py
@@ -1,10 +1,10 @@
+#!/usr/bin/python
+# ChangeIdracUserPasswordREDFISH. Python script using Redfish API with OEM extension to change iDRAC username password. Once the password is changed, the script will also execute a GET command to verify the password change was successful.
 #
-# ChangeIdracUserPasswordREDFISH. Python script using Redfish API to change iDRAC username password. Once the password is changed, the script will also execute a GET command to verify the password change was successful.
-#
-# NOTE: For iDRAC account ID parameter, pass in the ID of the user. Example: If changing root password, pass in a value of 2 for account ID parameter.
+# 
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 1.0
+# _version_ = 3.0
 #
 # Copyright (c) 2017, Dell, Inc.
 #
@@ -16,52 +16,101 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
 
-import requests, json, sys, re, time, warnings
+import requests, json, sys, re, time, warnings, argparse
 
 from datetime import datetime
 
 warnings.filterwarnings("ignore")
 
-try:
-    idrac_ip = sys.argv[1]
-    idrac_username = sys.argv[2]
-    idrac_password = sys.argv[3]
-    idrac_account_id = sys.argv[4]
-    idrac_new_password = sys.argv[5]
-   
-except:
-    print("- FAIL: You must pass in script name along with iDRAC IP / iDRAC username / iDRAC password / idrac_account_id / idrac_new_password. Example: \"script_name.py 192.168.0.120 root calvin 2 test\"")
-    sys.exit()
+
+parser=argparse.ArgumentParser(description="Python script using Redfish API with OEM extension to change iDRAC username password. Once the password is changed, the script will also execute a GET command to verify the password change was successful.")
+parser.add_argument('script_examples',action="store_true",help='ChangeIdracUserPasswordREDFISH.py -ip 192.168.0.120 -u root -p calvin -g y, this example will get user account information for all iDRAC users. ChangeIdracUserPasswordREDFISH.py -ip 192.168.0.120 -u user -p calvin -id 3 -np pAssw0rd, this example shows changing iDRAC user ID 3 password.')
+parser.add_argument('-ip',help='iDRAC IP address', required=True)
+parser.add_argument('-u', help='iDRAC username', required=True)
+parser.add_argument('-p', help='iDRAC password', required=True)
+parser.add_argument('-g', help='Get iDRAC user account information, pass in \"y\". This will return detailed information for each iDRAC user account.', required=False)
+parser.add_argument('-id', help='Pass in the iDRAC user account ID you want to change the password for. If needed, use argument -g to get the iDRAC user account ID.', required=False)
+parser.add_argument('-np', help='Pass in the new password you want to set for the iDRAC user ID.', required=False)
+
+args=vars(parser.parse_args())
+
+idrac_ip=args["ip"]
+idrac_username=args["u"]
+idrac_password=args["p"]
+
+### Function to get iDRAC user accounts information
+
+def get_iDRAC_user_account_info():
+    response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Accounts' % (idrac_ip),verify=False,auth=(idrac_username, idrac_password))
+    statusCode = response.status_code
+    if statusCode == 200:
+        pass
+    else:
+        data = response.json()
+        print("\n- FAIL, status code %s returned for GET command. Detail error results: \n%s" % (statusCode, data))
+        sys.exit()
+    data = response.json()
+    print("\n- iDRAC User Account Information -")
+    for i in data["Members"]:
+        for ii in i.items():
+            remove_id_1 = ii[1].strip("/")[-1]
+            if remove_id_1 == "1":
+                continue
+            else:
+                response = requests.get('https://%s%s' % (idrac_ip, ii[1]),verify=False,auth=(idrac_username, idrac_password))
+                statusCode = response.status_code
+                if statusCode == 200:
+                    pass
+                else:
+                    data = response.json()
+                    print("\n- FAIL, status code %s returned for GET command. Detail error results: \n%s" % (statusCode, data))
+                    sys.exit()
+                data=response.json()
+                print("\n")
+                for i in data.items():
+                    if "@" in i[0] or i[0] == "Links":
+                        pass
+                    else:
+                        print("%s: %s" % (i[0], i[1]))
+                
 
 
 ### Function to change iDRAC user password and verify password was changed by executing GET command with new password
 
 def set_idrac_user_password():
-    
     url = 'https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Accounts/%s' % (idrac_ip, idrac_account_id)
-    
     payload = {'Password': idrac_new_password}
     headers = {'content-type': 'application/json'}
     response = requests.patch(url, data=json.dumps(payload), headers=headers,verify=False, auth=(idrac_username, idrac_password))
 
     statusCode = response.status_code
     if statusCode == 200:
-        print("\n- PASS, status code %s returned for PATCH command to change iDRAC user password") % statusCode
+        print("\n- PASS, status code %s returned for PATCH command to change iDRAC user password" % statusCode)
     else:
-        print("\n- FAIL, status code %s returned, password was not changed") % statusCode
+        data = response.json()
+        print("\n- FAIL, status code %s returned, password was not changed. Detailed error results: \n%s" % (statusCode, data))
         sys.exit()
-    time.sleep(5)
+    print("- WARNING, executing GET request using new password for validating successful change")
+    time.sleep(10)
     response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Accounts/%s' % (idrac_ip, idrac_account_id),verify=False,auth=(idrac_username, idrac_new_password))
     
     statusCode = response.status_code
     if statusCode == 200:
-        print("\n- PASS, status code %s returned for GET command, iDRAC user password change success") % statusCode
+        print("\n- PASS, status code %s returned for GET command, iDRAC user password change success" % statusCode)
     else:
-        print("\n- FAIL, status code %s returned for GET command") % statusCode
+        data = response.json()
+        print("\n- FAIL, status code %s returned for GET command. Detail error results: \n%s" % (statusCode, data))
         sys.exit()
     
 ### Run code
 
 if __name__ == "__main__":
-    set_idrac_user_password()
+    if args["g"]:
+        get_iDRAC_user_account_info()
+    elif args["id"] and args["np"]:
+        idrac_account_id = args["id"]
+        idrac_new_password = args["np"]
+        set_idrac_user_password()
+    else:
+        print("\n- FAIL, either missing parameter(s) or invalid parameter value(s) passed in. If needed, review help text for script examples")
 
