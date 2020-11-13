5a7d5516027963f109abc7224537e88c2181f2a7
texroemer <30351677+texroemer@users.noreply.github.com>
2020-11-13 13:21:41 -0600

    Add support get UEFI device path for boot devices

--- Redfish Python/SetNextOneTimeBootDeviceREDFISH.py
+++ Redfish Python/SetNextOneTimeBootDeviceREDFISH.py
@@ -3,7 +3,7 @@
 #
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 4.0
+# _version_ = 5.0
 #
 # Copyright (c) 2017, Dell, Inc.
 #
@@ -28,9 +28,10 @@ parser.add_argument('-ip', help='iDRAC IP Address', required=True)
 parser.add_argument('-u', help='iDRAC username', required=True)
 parser.add_argument('-p', help='iDRAC username pasword', required=True)
 parser.add_argument('script_examples',action="store_true",help='SetNextOneTimeBootDeviceREDFISH -ip 191.268.0.120 -u root -p calvin -c, this will get the current next boot setting and possible values.\n- SetNextOneTimeBootDeviceREDFISH -ip 192.168.0.10 -u root -p calvin -o Pxe -r y, this will set next one time boot to PXE and reboot the server now. Once the system completes POST, system will PXE boot.\n- SetNextOneTimeBootDeviceREDFISH.py -ip 192.168.0.120 -u root -p calvin -o UefiTarget -U VenHw(986D1755-B9D0-4F8D-A0DA-D1DB18672045) -r y. This example will reboot the server and one time boot to the Uefi target vendor device ID which is HTTP device path. The vendor device ID string information is in F2 UEFI Device Manager.')
-parser.add_argument('-c', help='user option, pass in \"y\" to get current next boot onetime boot setting and possible values', required=False)
-parser.add_argument('-o', help='user option, pass in the string onetime boot device you want to set for next reboot. NOTE: This value is case sensitive so pass in exact value as stated in possible values for -c option', required=False, type=str)
-parser.add_argument('-U', help='user option to set UEFI target path. This will be used with -o option if you pass in UefiTarget value. ', required=False)
+parser.add_argument('-c', help='Pass in \"y\" to get current next boot onetime boot setting and possible values', required=False)
+parser.add_argument('-o', help='Pass in the string onetime boot device you want to set for next reboot. NOTE: This value is case sensitive so pass in exact value as stated in possible values for -c option', required=False, type=str)
+parser.add_argument('-U', help='Set UEFI target path. This will be used with -o option if you pass in UefiTarget value. ', required=False)
+parser.add_argument('-g', help='Get UEFI target path values for each boot order entry, pass in \"y\".', required=False)
 parser.add_argument('-r', help='user option, pass in \"y\" if you want the server to reboot now once you set next boot onetime boot device or \"n\" to not reboot now', required=False)
 args=vars(parser.parse_args())
 
@@ -38,15 +39,52 @@ idrac_ip=args["ip"]
 idrac_username=args["u"]
 idrac_password=args["p"]
 
+### Function to check supported iDRAC version
+
+def check_supported_idrac_version():
+    response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        print("\n- WARNING, status code %s returned. Incorrect iDRAC username/password or invalid privilege detected." % response.status_code)
+        sys.exit()
+    elif response.status_code != 200:
+        print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        sys.exit()
+    else:
+        pass
+
+### Function to get UEFI target path info for boot order devices
+
+def get_uefi_target_path_values():
+    response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/BootOptions?$expand=*($levels=1)' % idrac_ip,verify=False,auth=(idrac_username,idrac_password))
+    if response.status_code != 200:
+        print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        sys.exit()
+    else:
+        pass
+    data = response.json()
+    if data["Members"] == []:
+        print("- WARNING, no boot devices detected in the boot order")
+        sys.exit()
+    print("\n")
+    for i in data["Members"]:
+        for ii in i.items():
+            if ii[0] == "DisplayName" or ii[0] == "Id" or ii[0] == "UefiDevicePath":
+                print("%s: %s" % (ii[0], ii[1]))
+            else:
+                pass
+        print("\n")
+    sys.exit()
+
 ### Function to get current next boot onetime boot setting possible values for onetime boot
 
 def get_current_setting_next_boot_supported_values():
     response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
     data = response.json()
     print("\n-Supported values for next server reboot, one time boot:\n")
-    for i in data[u'Boot'][u'BootSourceOverrideTarget@Redfish.AllowableValues']:
+    for i in data['Boot']['BootSourceOverrideTarget@Redfish.AllowableValues']:
       print(i)
-    print("\n- Current next reboot, one time boot setting is: %s" % data[u'Boot'][u'BootSourceOverrideTarget'])
+    print("\n- Current next reboot, one time boot setting is: %s" % data['Boot']['BootSourceOverrideTarget'])
     sys.exit()
     
 ### Function to set next boot onetime boot device
@@ -56,7 +94,7 @@ def set_next_boot_onetime_boot_device():
     if args["o"] == "UefiTarget" and args["U"]:
       payload = {"Boot":{"BootSourceOverrideTarget":args["o"],"UefiTargetBootSourceOverride":args["U"]}}
     else:
-      payload = {"Boot":{"BootSourceOverrideTarget":args["o"]}} 
+      payload = {"Boot":{"BootSourceOverrideTarget":args["o"]}}
     headers = {'content-type': 'application/json'}
     response = requests.patch(url, data=json.dumps(payload), headers=headers, verify=False,auth=(idrac_username, idrac_password))
     data = response.json()
@@ -68,7 +106,7 @@ def set_next_boot_onetime_boot_device():
       else:
         print("\n- PASS, PATCH command passed to set next boot onetime boot device to: \"%s\"" % args["o"])
     else:
-      print("\n- FAIL, Command failed, error code is %s" % statusCode)
+      print("\n- FAIL, Command failed, errror code is %s" % statusCode)
       detail_message=str(response.__dict__)
       print(detail_message)
       sys.exit()
@@ -79,8 +117,8 @@ def set_next_boot_onetime_boot_device():
 def reboot_server():
     response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
     data = response.json()
-    print("\n- WARNING, Current server power state is: %s" % data[u'PowerState'])
-    if data[u'PowerState'] == "On":
+    print("\n- INFO, Current server power state is: %s" % data['PowerState'])
+    if data['PowerState'] == "On":
         url = 'https://%s/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset' % idrac_ip
         payload = {'ResetType': 'GracefulShutdown'}
         headers = {'content-type': 'application/json'}
@@ -97,11 +135,11 @@ def reboot_server():
         while True:
             response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
             data = response.json()
-            if data[u'PowerState'] == "Off":
+            if data['PowerState'] == "Off":
                 print("- PASS, GET command passed to verify server is in OFF state")
                 break
             elif count == 20:
-                print("- WARNING, unable to graceful shutdown the server, will perform forced shutdown now")
+                print("- INFO, unable to graceful shutdown the server, will perform forced shutdown now")
                 url = 'https://%s/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset' % idrac_ip
                 payload = {'ResetType': 'ForceOff'}
                 headers = {'content-type': 'application/json'}
@@ -130,7 +168,7 @@ def reboot_server():
             print("\n- FAIL, Command failed to power ON server, status code is: %s\n" % statusCode)
             print("Extended Info Message: {0}".format(response.json()))
             sys.exit()
-    elif data[u'PowerState'] == "Off":
+    elif data['PowerState'] == "Off":
         url = 'https://%s/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset' % idrac_ip
         payload = {'ResetType': 'On'}
         headers = {'content-type': 'application/json'}
@@ -151,21 +189,21 @@ def reboot_server():
 ### Run code
 
 if __name__ == "__main__":
-  if args["c"]:
-    get_current_setting_next_boot_supported_values()
-  elif args["o"] and args["r"]:
-    set_next_boot_onetime_boot_device()
+    check_supported_idrac_version()
+    if args["c"]:
+        get_current_setting_next_boot_supported_values()
+    elif args["g"]:
+        get_uefi_target_path_values()
+    elif args["o"] and args["r"]:
+        set_next_boot_onetime_boot_device()
     if args["r"] == "n":
-      print("- WARNING, no reboot selected for -r argument. Onetime boot will be applied on next manual server reboot")
-      sys.exit()
+        print("- INFO, no reboot selected for -r argument. Onetime boot will be applied on next manual server reboot")
+        sys.exit()
     elif args["r"] == "y":
         reboot_server()
         print("\n- System will now complete POST and one time boot to \"%s\"" % args["o"])
     else:
-        print("- FAIL, invalid value passed in for -r argument. Check script help for supported values")
-        sys.exit()
-  else:
-      print("\n- FAIL, either missing or invalid parameter(s) passed in. If needed, see script help text for supported parameters and script examples")
+        print("\n- FAIL, either missing or invalid parameter(s) passed in. If needed, see script help text for supported parameters and script examples")
 
 
 
