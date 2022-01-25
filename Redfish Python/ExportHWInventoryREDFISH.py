f61ca221d2ac3e05ecaf995067764738a5881a7e
texroemer <30351677+texroemer@users.noreply.github.com>
2022-01-25 13:47:24 -0600

    Add files via upload

--- Redfish Python/ExportHWInventoryREDFISH.py
+++ Redfish Python/ExportHWInventoryREDFISH.py
@@ -2,7 +2,7 @@
 # ExportHWInventoryREDFISH. Python script using Redfish API with OEM extension to export server hardware(HW) inventory to either local directory or network share
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 4.0
+# _version_ = 5.0
 #
 # Copyright (c) 2019, Dell, Inc.
 #
@@ -30,11 +30,11 @@ parser.add_argument('-s', help='Get supported network share types, pass in \"y\"
 parser.add_argument('--ipaddress', help='Pass in the IP address of the network share', required=False)
 parser.add_argument('--sharetype', help='Pass in the share type of the network share to export or pass in "Local" to perform local export. For supported network share types, execute -s argument. NOTE: When passing in Local for share type, you don\'t need to pass in any other arguments. This exported HW inventory file will be in XML format.', required=False)
 parser.add_argument('--sharename', help='Pass in the network share share name', required=False)
-parser.add_argument('--username', help='Pass in the CIFS username', required=False)
-parser.add_argument('--password', help='Pass in the CIFS username pasword', required=False)
+parser.add_argument('--username', help='Pass in the network username', required=False)
+parser.add_argument('--password', help='Pass in the network username pasword', required=False)
 parser.add_argument('--workgroup', help='Pass in the workgroup of your CIFS network share. This argument is optional', required=False)
-parser.add_argument('--filename', help='Pass in unique file name string for the export Hw inventory file', required=False)
-parser.add_argument('--ignorecertwarning', help='Supported values are Off and On. This argument is only required if using HTTPS for share type', required=False)
+parser.add_argument('--filename', help='Pass in unique file name string for export HW inventory file. This argument is only required for exporting to network share, not required for local export.', required=False)
+parser.add_argument('--ignorecertwarning', help='Supported values are Off and On. This argument is only supported for HTTPS share type', required=False)
 
 
 
@@ -50,20 +50,14 @@ def check_supported_idrac_version():
     if response.__dict__['reason'] == "Unauthorized":
         print("\n- FAIL, unauthorized to execute Redfish command. Check to make sure you are passing in correct iDRAC username/password and the IDRAC user has the correct privileges")
         sys.exit()
-    else:
-        pass
     data = response.json()
     supported = "no"
     for i in data['Actions'].keys():
         if "ExportHWInventory" in i:
             supported = "yes"
-        else:
-            pass
     if supported == "no":
         print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
         sys.exit()
-    else:
-        pass
 
 
 def get_supported_network_share_types():
@@ -83,11 +77,8 @@ def export_hw_inventory():
     global job_id
     url = 'https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService/Actions/DellLCService.ExportHWInventory' % (idrac_ip)
     method = "ExportHWInventory"
+    payload={}   
     headers = {'content-type': 'application/json'}
-    payload={}
-        
-    headers = {'content-type': 'application/json'}
-    payload={}
     if args["ipaddress"]:
         payload["IPAddress"] = args["ipaddress"]
     if args["sharetype"]:
@@ -107,12 +98,6 @@ def export_hw_inventory():
         payload["Workgroup"] = args["workgroup"]
     if args["ignorecertwarning"]:
         payload["IgnoreCertWarning"] = args["ignorecertwarning"]
-    print("\n- WARNING, arguments and values for %s method\n" % method)
-    for i in payload.items():
-        if i[0] == "Password":
-            print("Password: ********")
-        else:
-            print("%s: %s" % (i[0],i[1]))
     response = requests.post(url, data=json.dumps(payload), headers=headers, verify=False,auth=(idrac_username,idrac_password))
     data = response.json()
     if response.status_code == 202:
@@ -126,7 +111,7 @@ def export_hw_inventory():
         get_service_tag = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1' % (idrac_ip),verify=False,auth=(idrac_username, idrac_password))
         data_get_service_tag = get_service_tag.json()
         chassis_service_tag = data_get_service_tag['Oem']['Dell']['DellSystem']['NodeID']
-        print("- WARNING, Redfish export HW inventory URI: \"%s\"\n" % response.headers['Location'])
+        print("- INFO, Redfish export HW inventory URI: \"%s\"\n" % response.headers['Location'])
         response = requests.get('https://%s%s' % (idrac_ip, response.headers['Location']),verify=False,auth=(idrac_username, idrac_password))
         get_datetime_info = datetime.now()
         export_filename = "%s-%s-%s_%s%s%s_export_HW_inventory_%s.xml"% (get_datetime_info.year, get_datetime_info.month, get_datetime_info.day, get_datetime_info.hour, get_datetime_info.minute, get_datetime_info.second, chassis_service_tag)
@@ -168,11 +153,11 @@ def loop_job_status():
         if str(current_time)[0:7] >= "0:05:00":
             print("\n- FAIL: Timeout of 5 minutes has been hit, script stopped\n")
             sys.exit()
-        elif "Fail" in data[u'Message'] or "fail" in data[u'Message'] or data[u'JobState'] == "Failed" or "Unable" in data[u'Message']:
-            print("- FAIL: job ID %s failed, failed message is: %s" % (job_id, data[u'Message']))
+        elif "Fail" in data['Message'] or "fail" in data['Message'] or data['JobState'] == "Failed" or "Unable" in data['Message']:
+            print("- FAIL: job ID %s failed, failed message is: %s" % (job_id, data['Message']))
             sys.exit()
-        elif data[u'JobState'] == "Completed":
-            if data[u'Message'] == "Hardware Inventory Export was successful":
+        elif data['JobState'] == "Completed":
+            if data['Message'] == "Hardware Inventory Export was successful":
                 print("\n--- PASS, Final Detailed Job Status Results ---\n")
             else:
                 print("\n--- FAIL, Final Detailed Job Status Results ---\n")
@@ -183,7 +168,7 @@ def loop_job_status():
                     print("%s: %s" % (i[0],i[1]))
             break
         else:
-            print("- WARNING, job state not marked completed, current job status is running, polling again")
+            print("- INFO, job state not marked completed, current job status is running, polling again")
             
 
     
