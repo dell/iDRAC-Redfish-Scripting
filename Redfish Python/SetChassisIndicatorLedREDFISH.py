be2103e5952f778d7a1afbf81297fa583cab64b4
texroemer <30351677+texroemer@users.noreply.github.com>
2022-02-28 15:37:20 -0600

    Multiple fixes
    - Fixed error with print statement
    - added logging support
    - added script examples
    - made PEP compliant

--- Redfish Python/SetChassisIndicatorLedREDFISH.py
+++ Redfish Python/SetChassisIndicatorLedREDFISH.py
@@ -2,7 +2,7 @@
 # SetChassisIndicatorledREDFISH. Python script using Redfish API to either get current chassis indicator LED state or set chassis indicator LED state.
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 1.0
+# _version_ = 2.0
 #
 # Copyright (c) 2018, Dell, Inc.
 #
@@ -14,45 +14,70 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
 
-import requests, json, sys, re, time, warnings, argparse
+import argparse
+import json
+import logging
+import re
+import requests
+import sys
+import time
+import warnings
 
 warnings.filterwarnings("ignore")
 
 parser=argparse.ArgumentParser(description="Python script using Redfish API to either get current chassis indicator LED state or set chassis indicator LED state")
-parser.add_argument('-ip',help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-parser.add_argument('-g', help='Get current chassis indicator LED state, pass in \"y\"', required=False)
-parser.add_argument('-s', help='Set chassis indicator LED state, pass in one of the supported values: \"Off\" or \"Blinking\". These values are case sensitive so pass in the exact string syntax', required=False)
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password', required=False)
+parser.add_argument('--script-examples', help='Get examples of executing script.', action="store_true", dest="script_examples", required=False)
+parser.add_argument('--get', help='Get current chassis indicator LED state', action="store_true", required=False)
+parser.add_argument('--set', help='Set chassis indicator LED state, pass in one of the supported values: \"Lit\" or \"Blinking\". These values are case sensitive so pass in the exact string syntax', required=False)
 args=vars(parser.parse_args())
+
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
 idrac_ip=args["ip"]
 idrac_username=args["u"]
 idrac_password=args["p"]
 
+def script_examples():
+    print("""SetChassisIndicatorLedREDFISH.py -ip 192.168.0.120 -u root -p calvin --get, this example will return current chassis LED state.
+    \nSetChassisIndicatorLedREDFISH.py -ip 192.168.0.120 -u root -p calvin --set Lit, this example will disable blinking and set chassis LED state to Lit.
+    \nSetChassisIndicatorLedREDFISH.py -ip 192.168.0.120 -u root -p calvin --set Blinking, this example will set chassis LED to blink.""")
+
 def get_current_chassis_indicator_LED_state():
     response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
     data = response.json()
-    print("\n- WARNING, current chassis indicator LED state is: %s" % data[u'IndicatorLED'])
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code 401 detected, check iDRAC username / password credentials")
+        sys.exit(0)
+    elif response.status_code != 200:
+        logging.error("- ERROR, status code %s returned, error results: %s" % (response.status_code, data))
+        sys.exit(0)
+    logging.info("\n- INFO, current chassis indicator LED state is: %s" % data['IndicatorLED'])
 
 def set_current_chassis_indicator_LED_state():
     url = 'https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip
-    payload = {'IndicatorLED': args["s"]}
+    payload = {'IndicatorLED': args["set"]}
     headers = {'content-type': 'application/json'}
     response = requests.patch(url, data=json.dumps(payload), headers=headers, verify=False, auth=(idrac_username,idrac_password))
-    #print(response.__dict__)
     if response.status_code == 200:
-        print("\n- PASS, PATCH command successfully completed \"%s\" request for chassis indicator LED" % args["s"])
+        logging.info("\n- PASS, PATCH command successfully completed \"%s\" request for chassis indicator LED" % args["set"])
     else:
-        print("\n- FAIL, status code %s returned, detailed failure results:\n" % response.status_code)
-        print(response.__dict__)
+        logging.error("\n- ERROR, status code %s returned, detailed failure results:\n%s" % (response.status_code, response.__dict__))
+        sys.exit(0)
         
 
 if __name__ == "__main__":
-    if args["g"]:
+    if args["script_examples"]:
+        script_examples()
+    elif args["get"]:
         get_current_chassis_indicator_LED_state()
-    elif args["s"]:
+    elif args["set"]:
         set_current_chassis_indicator_LED_state()
+    else:
+        script_examples()
+        
         
 
 
