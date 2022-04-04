473d95565e41bb5c4a6cef8dba57ce899f19c1bf
texroemer <30351677+texroemer@users.noreply.github.com>
2022-04-04 13:27:31 -0700

    Multiple updates
    - Updated Code to be PEP compliant
    - Updated script examples 
    - Updated argument naming, easier to understand
    - Added x-auth token support to run redfish calls
    - Argument -p not passed in, script will prompt to enter user password, not echo to the screen
    - Added optional argument to check SSL cert for all Redfish calls. Default behavior is to not check for any Redfish calls

--- Redfish Python/GetSetPowerStateREDFISH.py
+++ Redfish Python/GetSetPowerStateREDFISH.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python
+#!/usr/bin/python3
 #
 # GetSetPowerStateREDFISH. Python script using Redfish API to change current server power state.
 #
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 7.0
+# _version_ = 8.0
 #
-# Copyright (c) 2022, Dell, Inc.
+# Copyright (c) 2021, Dell, Inc.
 #
 # This software is licensed to you under the GNU General Public License,
 # version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -17,85 +19,109 @@
 
 
 import argparse
+import getpass
 import json
+import logging
 import requests
 import sys
 import warnings
 
 warnings.filterwarnings("ignore")
 
-parser = argparse.ArgumentParser(
-    description="Python script using Redfish API to either get current server power state and possible power state "
-                "values or execute server power state change")
-parser.add_argument('-ip', help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-mutex_group = parser.add_mutually_exclusive_group(required=True)
-mutex_group.add_argument('--script-examples', action="store_true",
-                         help='Prints a set of examples for this script.')
-mutex_group.add_argument('-g',
-                         help='Get current power state of the server and possible values for ComputerSystem.Reset.',
-                         action='store_true')
-mutex_group.add_argument('-r',
-                         help='Pass in the computer system reset type you want to perform. To get supported possible '
-                              'values, execute the script with -g argument')
+parser = argparse.ArgumentParser(description="Python script using Redfish API to either get current server power state and possible power state values or execute server power state change")
+parser.add_argument('-ip', help='Pass in iDRAC IP address', required=False)
+parser.add_argument('-u', help='Pass in iDRAC username', required=False)
+parser.add_argument('-p', help='Pass in iDRAC password. If not passed in, script will prompt to enter password which will not be echoed to the screen', required=False)
+parser.add_argument('--ssl', help='Verify SSL certificate for all Redfish calls, pass in \"true\". This argument is optional, if you do not pass in this argument, all Redfish calls will ignore SSL cert checks.', required=False)
+parser.add_argument('-x', help='Pass in iDRAC X-auth token session ID to execute all Redfish calls instead of passing in username/password', required=False)
+parser.add_argument('--script-examples', help='Get executing script examples', action="store_true", dest="script_examples", required=False)
+parser.add_argument('--get', help='Get current server power state and possible values to set server power state', action="store_true", required=False)
+parser.add_argument('--set', help='Set server power state', required=False)
+
 
 args = vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
-idrac_ip = args["ip"]
-idrac_username = args["u"]
-idrac_password = args["p"]
+def script_examples():
+    print("""\n- GetSetPowerStateREDFISH.py -ip 192.168.0.120 -u root -p calvin --get, this example will return current server power state and possible values supported to set power state.
+    \n- GetSetPowerStateREDFISH.py -ip 192.168.0.120 -u root -p calvin --set ForceRestart, this example will set server power state to ForceRestart.
+    \n- GetSetPowerStateREDFISH.py -ip 192.168.0.120 -u root -p calvin --set ForceOff --ssl true, this example uses ssl cert validation for Redfish calls to set server power state to Off.
+    \n- GetSetPowerStateREDFISH.py -ip 192.168.0.120 --set On -x 6695c2c7069a3369c6f90e34f5582xv3, this example uses X-auth token session to set server power state to On.""")
+    sys.exit(0)
 
-def test_idrac_creds():
-    response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1' % (idrac_ip), auth=(idrac_username, idrac_password), verify=False)
-    if response.status_code == 401:
-        print("\n- WARNING, status code 401 detected, check iDRAC username / password credentials")
-        sys.exit(1)
+def check_supported_idrac_version():
+    if args["x"]:
+         response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
     else:
-        pass
-
-
-def get_current_power_state():
-    response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/' % idrac_ip, verify=False,
-                            auth=(idrac_username, idrac_password))
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
     data = response.json()
-    print("\n- INFO, Current server power state is: %s\n" % data['PowerState'])
-    print("- Supported values for server power control are:\n")
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code 401 detected, check iDRAC username / password credentials")
+        sys.exit(0)
+    elif response.status_code != 200:
+        logging.warning("\n- WARNING, GET request failed to validate iDRAC creds, status code %s returned." % response.status_code)
+        logging.warning(data)
+        sys.exit(0)
+    
+def get_current_power_state():
+    if args["x"]:
+         response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code != 200:
+        logging.warning("\n- WARNING, GET request failed to get current server power state, status code %s returned." % response.status_code)
+        logging.warning(data)
+        sys.exit(0)
+    logging.info("\n- INFO, Current server power state: %s\n" % data['PowerState'])
+    logging.info("- Supported values to set server power state:\n")
     for i in data['Actions']['#ComputerSystem.Reset']['ResetType@Redfish.AllowableValues']:
         print(i)
 
-
 def set_power_state():
-    requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/' % idrac_ip, verify=False,
-                 auth=(idrac_username, idrac_password))
-    print("\n- INFO, setting new server power state to: %s" % (args["r"]))
-
+    logging.info("\n- INFO, setting new server power state to: %s" % (args["set"]))
     url = 'https://%s/redfish/v1/Systems/System.Embedded.1/Actions/ComputerSystem.Reset' % idrac_ip
-    payload = {'ResetType': args["r"]}
-    headers = {'content-type': 'application/json'}
-    response = requests.post(url, data=json.dumps(payload), headers=headers, verify=False,
-                             auth=(idrac_username, idrac_password))
-
-    status_code = response.status_code
-    if status_code == 204:
-        print("\n- PASS, status code %s returned, server power state successfully set to \"%s\"\n" % (
-            status_code, args["r"]))
+    payload = {'ResetType': args["set"]}
+    if args["x"]:
+        headers = {'content-type': 'application/json', 'X-Auth-Token': args["x"]}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert)
     else:
-        print("\n- FAIL, Command failed, status code %s returned\n" % status_code)
-        print(response.json())
-        sys.exit(1)
+        headers = {'content-type': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert, auth=(idrac_username, idrac_password))
+    if response.status_code == 204:
+        logging.info("\n- PASS, status code %s returned, server power state successfully set to \"%s\"\n" % (response.status_code, args["set"]))
+    else:
+        logging.error("\n- FAIL, Command failed, status code %s returned\n" % response.status_code)
+        logging.error(response.json())
+        sys.exit(0)
 
 
 if __name__ == "__main__":
-    test_idrac_creds()
-    if args["g"]:
-        get_current_power_state()
-    elif args["r"]:
-        set_power_state()
-    elif args["script_examples"]:
-        print('\nGetSetPowerStateREDFISH.py -ip 192.168.0.120 -u root -p calvin -g, this example will '
-              'return the current power state of the server and supported values for changing the '
-              'server power state. \n\nSetPowerStateREDFISH.py -ip 192.168.0.120 -u root -p calvin -r On,'
-              ' this example will power on the server.')
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] and args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+                verify_cert = False
+        check_supported_idrac_version()
     else:
-        print("- FAIL, incorrect parameter(s) passed in or missing required parameters")
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    if args["get"]:
+        get_current_power_state()
+    elif args["set"]:
+        set_power_state()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
