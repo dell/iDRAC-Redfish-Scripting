3bca132a410752174eee8039c97e8861f90fb142
texroemer <30351677+texroemer@users.noreply.github.com>
2022-04-07 10:43:01 -0700

    Multiple updates
    - Updated Code to be PEP compliant
    - Updated script examples 
    - Updated argument naming, easier to understand
    - Added x-auth token support to run redfish calls
    - Argument -p not passed in, script will prompt to enter user password, not echo to the screen
    - Added optional argument to check SSL cert for all Redfish calls. Default behavior is to not check for any Redfish calls

--- Redfish Python/GetRemoteServicesAPIStatusREDFISH.py
+++ Redfish Python/GetRemoteServicesAPIStatusREDFISH.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+#!/usr/bin/python3
 #
 # GetRemoteServicesAPIStatusREDFISH. Python script using Redfish API with OEM extension to get the server remote services status
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 2.0
+# _version_ = 4.0
 #
 # Copyright (c) 2019, Dell, Inc.
 #
@@ -15,71 +17,92 @@
 #
 
 
-import requests, json, sys, re, time, warnings, argparse
+import argparse
+import getpass
+import json
+import logging
+import requests
+import sys
+import warnings
 
 from datetime import datetime
+from pprint import pprint
 
 warnings.filterwarnings("ignore")
 
-parser=argparse.ArgumentParser(description="Python script using Redfish API with OEM extension to get the server remote services status. This will return: lifecycle controller(LC) status, real time monitoring (RT) status, server status and overall status of the command")
-parser.add_argument('-ip',help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-
-
-
+parser=argparse.ArgumentParser(description="Python script using Redfish API with OEM extension to get the server remote services status. This will return: lifecycle controller(LC) status, real time monitoring (RT) status, server status (examples: In POST, out of POST, in F2 Settings), Telemetry server status (if supported) and overall status.")
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in \"y\". By default, the script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', help='Get executing script examples', action="store_true", dest="script_examples", required=False)
 
 args=vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
-idrac_ip=args["ip"]
-idrac_username=args["u"]
-idrac_password=args["p"]
-
+def script_examples():
+    print("""\n- GetRemoteServicesAPIStatusREDFISH.py -ip 192.168.0.120 -u root -p calvin, this example will return lifecycle controller(LC) status, real time monitoring (RT) status, server status, Telemetry server status (if supported) and overall status.""")
+    sys.exit(0)
 
 def check_supported_idrac_version():
-    response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    try:
-        data = response.json()
-    except:
-        print("\n- FAIL, either incorrect iDRAC username / password passed in or iDRAC user doesn't have correct privileges")
-        sys.exit()
-    if response.status_code == 401:
-        print("\n- WARNING, status code 401 detected, check iDRAC username / password credentials")
-        sys.exit()
-    if response.status_code != 200:
-        print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
-        sys.exit()
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
     else:
-        pass
-
-
-
+        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert,auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned. Incorrect iDRAC username/password or invalid privilege detected." % response.status_code)
+        sys.exit(0)
+    if response.status_code != 200:
+        logging.warning("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        sys.exit(0)
 
 def get_remote_service_api_status():
     url = 'https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService/Actions/DellLCService.GetRemoteServicesAPIStatus' % (idrac_ip)
     method = "GetRemoteServicesAPIStatus"
-    headers = {'content-type': 'application/json'}
     payload={}
-    response = requests.post(url, data=json.dumps(payload), headers=headers, verify=False,auth=(idrac_username,idrac_password))
+    if args["x"]:
+        headers = {'content-type': 'application/json', 'X-Auth-Token': args["x"]}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert)
+    else:
+        headers = {'content-type': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert,auth=(idrac_username,idrac_password))
     data=response.json()
     if response.status_code == 200:
-        print("\n-PASS: POST command passed for %s method, status code 200 returned\n" % method)
+        logging.info("\n- PASS: POST command passed for %s method, status code 200 returned\n" % method)
     else:
-        print("\n-FAIL, POST command failed for %s method, status code is %s" % (method, response.status_code))
+        logging.error("\n- FAIL, POST command failed for %s method, status code is %s" % (method, response.status_code))
         data = response.json()
-        print("\n-POST command failure results:\n %s" % data)
-        sys.exit()
+        logging.error("\n- POST command failure results:\n %s" % data)
+        sys.exit(0)
     for i in data.items():
-        if i[0] =="@Message.ExtendedInfo":
-            pass
-        else:
-            print("%s: %s" % (i[0], i[1]))
-    
+        pprint(i)
 
     
-
 if __name__ == "__main__":
-    check_supported_idrac_version()
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] and args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip=args["ip"]
+        idrac_username=args["u"]
+        if args["p"]:
+            idrac_password=args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+                verify_cert = False
+        check_supported_idrac_version()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
     get_remote_service_api_status()
     
     
