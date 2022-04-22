184591691f174bbdaca7236ed0f5eecbdd92bcb4
texroemer <30351677+texroemer@users.noreply.github.com>
2022-04-22 09:06:39 -0700

    Multiple updates
    - Updated code to be PEP compliant
    - Updated script examples 
    - Updated argument naming, easier to understand
    - Added x-auth token support to run redfish calls
    - Argument -p not passed in, script will prompt to enter user password, not echo to the screen
    - Added optional argument to check SSL cert for all Redfish calls. Default behavior is to not check for any Redfish calls

--- Redfish Python/GetIdracServerSlotInformationREDFISH.py
+++ Redfish Python/GetIdracServerSlotInformationREDFISH.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+#!/usr/bin/python3
 #
 # GetIdracServerSlotInformationREDFISH. Python script using Redfish API with OEM extension to get iDRAC server slot information.
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 4.0
+# _version_ = 5.0
 #
 # Copyright (c) 2020, Dell, Inc.
 #
@@ -14,105 +16,131 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
 
-import requests, json, sys, re, time, os, warnings, argparse
+import argparse
+import getpass
+import json
+import logging
+import os
+import re
+import requests
+import sys
+import time
+import warnings
 
 from datetime import datetime
+from pprint import pprint
 
 warnings.filterwarnings("ignore")
 
 parser=argparse.ArgumentParser(description="Python script using Redfish API with OEM extension to get server slot information. Slot information includes: Fan, CPU, DIMM, PCI, Backplane, PSU")
-parser.add_argument('script_examples',action="store_true",help='GetIdracServerSlotInformationREDFISH.py -ip 192.168.0.120 -u root -p calvin -s y, this example will get slot information for all server devices and also redirect output to a file. GetIdracServerSlotInformationREDFISH.py -ip 192.168.0.120 -u root -p calvin -x y, this example will convert output to XMl format, redirect to XML file.')
-parser.add_argument('-ip',help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-parser.add_argument('-s', help='Get all server slot information, pass in \"y\". This option will print information to the screen and also capture output in text file.', required=False)
-parser.add_argument('-x', help='Get all server slot information, pass in \"y\". This option will redirect slot information into XML format.', required=False)
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password. If you do not pass in argument -p, script will prompt to enter user password which will not be echoed to the screen.', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in value \"true\" or \"false\". By default, this argument is not required and script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', help='Get executing script examples', action="store_true", dest="script_examples", required=False)
+parser.add_argument('--get-text', help='Get all server slot information and echo output to the terminal, also redirect output to text file', dest="get_text", action="store_true", required=False)
+parser.add_argument('--get-xml', help='Get all server slot information and redirect to XML file. NOTE: This argument will not echo slot information to the terminal.', dest="get_xml", action="store_true", required=False)
+
 args=vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
-idrac_ip=args["ip"]
-idrac_username=args["u"]
-idrac_password=args["p"]
+def script_examples():
+    print("""\n- GetIdracServerSlotInformationREDFISH.py -ip 192.168.0.120 -u root -p calvin --get-text, this example will get slot information for all server devices and redirect output to text file.
+    \n- GetIdracServerSlotInformationREDFISH.py -ip 192.168.0.120 -u root -p calvin --get-xml, this example will get slot information for all server devices and redirect output to XML file.""")
+    sys.exit(0)
 
-
+def check_supported_idrac_version():
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
+    else:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' % idrac_ip, verify=verify_cert,auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned. Incorrect iDRAC username/password or invalid privilege detected." % response.status_code)
+        sys.exit(0)
+    if response.status_code != 200:
+        logging.warning("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        sys.exit(0)
 
 def get_server_slot_info():
     try:
-        os.remove(idrac_ip+"_server_slot_info.txt")
+        os.remove(idrac_ip + "_server_slot_info.txt")
     except:
         pass
-    f=open("%s_server_slot_info.txt" % idrac_ip,"a")
-    d=datetime.now()
-    current_date_time="- Data collection timestamp: %s-%s-%s  %s:%s:%s\n" % (d.month,d.day,d.year, d.hour,d.minute,d.second)
-    f.writelines(current_date_time)
-    f.writelines("\n\n")
-    response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' % idrac_ip,verify=False,auth=(idrac_username,idrac_password))
+    open_file = open("%s_server_slot_info.txt" % idrac_ip,"w")
+    get_datetime = datetime.now()
+    current_date_time = "- Data collection timestamp: %s-%s-%s  %s:%s:%s\n" % (get_datetime.month, get_datetime.day, get_datetime.year, get_datetime.hour, get_datetime.minute, get_datetime.second)
+    open_file.writelines(current_date_time)
+    open_file.writelines("\n\n")
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
+    else:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' % idrac_ip, verify=verify_cert,auth=(idrac_username, idrac_password))
     data = response.json()
-    if response.status_code == 200 or response.status_code == 202:
-        pass
-    else:
-        print("\n- FAIL, GET request failed, status code %s returned. Detailed error results: \n%s" % (response.status_code,data))
-        sys.exit()
+    if response.status_code != 200:
+        logging.error("\n- FAIL, GET request failed, status code %s returned. Detailed error results: \n%s" % (response.status_code,data))
+        sys.exit(0)
     if data['Members'] == []:
-        print("- FAIL, no data detected for Members property. Manually execute GET on URI 'https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' in browser to check. If no data detected, reboot server and run Collecting Inventory to refresh the configuration database for iDRAC, try GET again." % idrac_ip)
-        sys.exit()
-    else:
-        pass
-              
+        logging.error("- FAIL, no data detected for Members property. Manually execute GET on URI 'https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' in browser to check. If no data detected, reboot server and run Collecting Inventory to refresh the configuration database for iDRAC, try GET again." % idrac_ip)
+        sys.exit(0)
     for i in data['Members']:
+        pprint(i)
+        print("\n")
         for ii in i.items():
             server_slot_entry = ("%s: %s" % (ii[0],ii[1]))
-            print(server_slot_entry)
-            f.writelines("%s\n" % server_slot_entry)
-        print("\n")
-        f.writelines("\n")
-    number_list=[i for i in range (1,100001) if i % 50 == 0]
+            #print(server_slot_entry)
+            open_file.writelines("%s\n" % server_slot_entry)
+        #print("\n")
+        open_file.writelines("\n")
+    number_list = [i for i in range (1,100001) if i % 50 == 0]
     for seq in number_list:
-        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection?$skip=%s' % (idrac_ip, seq) ,verify=False,auth=(idrac_username,idrac_password))
+        if args["x"]:
+            response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection?$skip=%s' % (idrac_ip, seq), verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
+        else:
+            response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection?$skip=%s' % (idrac_ip, seq), verify=verify_cert,auth=(idrac_username, idrac_password))
         data = response.json()
-        if response.status_code == 200 or response.status_code == 202:
-            pass
-        else:
+        if response.status_code != 200:
             if "query parameter $skip is out of range" in data["error"]["@Message.ExtendedInfo"][0]["Message"]:
-                f.close()
-                print("\n- WARNING, iDRAC Server Slot Information also captured in \"%s_server_slot_info.txt\" file" % idrac_ip)
-                sys.exit()
+                open_file.close()
+                logging.info("\n- INFO, iDRAC Server Slot Information also captured in \"%s_server_slot_info.txt\" file" % idrac_ip)
+                sys.exit(0)
             else:
-                print("\n- FAIL, GET request failed using skip query parameter, status code %s returned. Detailed error results: \n%s" % (response.status_code,data))    
-                sys.exit()
-        if "Members" in data:
-            pass
-        else:
+                logging.error("\n- FAIL, GET request failed using skip query parameter, status code %s returned. Detailed error results: \n%s" % (response.status_code,data))    
+                sys.exit(0)
+        if "Members" not in data or data["Members"] == [] or response.status_code == 400:
             break
-        for i in data[u'Members']:
+        for i in data['Members']:
+            pprint(i)
+            print("\n")
             for ii in i.items():
                 server_slot_entry = ("%s: %s" % (ii[0],ii[1]))
-                print(server_slot_entry)
-                f.writelines("%s\n" % server_slot_entry)
-            print("\n")
-            f.writelines("\n")
-    print("\n- WARNING, iDRAC Server Slot Information also captured in \"%s_server_slot_info.txt\" file" % idrac_ip)
-    f.close()
+                #print(server_slot_entry)
+                open_file.writelines("%s\n" % server_slot_entry)
+            #print("\n")
+            open_file.writelines("\n")
+    logging.info("\n- INFO, iDRAC Server Slot Information also captured in \"%s_server_slot_info.txt\" file" % idrac_ip)
+    open_file.close()
 
 def get_server_slot_info_xml():
-    print("\n- WARNING, collecting server slot information and converting to XML format, copy to XML file")
+    logging.info("\n- INFO, collecting server slot information and converting to XML format, copy to XML file")
     try:
         os.remove(idrac_ip+"_server_slot_info.xml")
     except:
         pass
-    f=open("%s_server_slot_info.xml" % idrac_ip,"a")
-    f.writelines("<CIM>\n")
-    response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' % idrac_ip,verify=False,auth=(idrac_username,idrac_password))
+    open_file = open("%s_server_slot_info.xml" % idrac_ip,"a")
+    open_file.writelines("<CIM>\n")
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
+    else:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' % idrac_ip, verify=verify_cert,auth=(idrac_username, idrac_password))
     data = response.json()
-    if response.status_code == 200 or response.status_code == 202:
-        pass
-    else:
-        print("\n- FAIL, GET request failed, status code %s returned. Detailed error results: \n%s" % (response.status_code,data))
-        sys.exit()
+    if response.status_code != 200:
+        logging.error("\n- FAIL, GET request failed, status code %s returned. Detailed error results: \n%s" % (response.status_code,data))
+        sys.exit(0)
     if data['Members'] == []:
-        print("- FAIL, no data detected for Members property. Manually execute GET on URI 'https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' in browser to check. if no data detected, reboot server and run Collecting Inventory to refresh the configuration database for iDRAC" % idrac_ip)
-        sys.exit()
-    else:
-        pass
+        logging.error("- FAIL, no data detected for Members property. Manually execute GET on URI 'https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' in browser to check. if no data detected, reboot server and run Collecting Inventory to refresh the configuration database for iDRAC" % idrac_ip)
+        sys.exit(0)
     for i in data['Members']:
         create_dict = {}
         for ii in i.items():
@@ -126,31 +154,28 @@ def get_server_slot_info_xml():
                 else:
                     create_dict["Slot Number"] = str(ii[1])
         create_string = "<VALUE.NAMEDINSTANCE>\n<INSTANCENAME DEVICENAME=\""+create_dict["Id"]+"\">\n<KEYBINDING PROPERTY=\"Slot Number\">\n<VALUE>"+create_dict["Slot Number"]+"</VALUE>\n</KEYBINDING>\n</INSTANCENAME>\n<PROPERTY PROPERTY=\"EmptySlot\">\n<VALUE>"+create_dict["EmptySlot"]+"</VALUE>\n</PROPERTY>\n</VALUE.NAMEDINSTANCE>"  
-        f.writelines(create_string)
-    number_list=[i for i in range (1,100001) if i % 50 == 0]
+        open_file.writelines(create_string)
+    number_list = [i for i in range (1,100001) if i % 50 == 0]
     for seq in number_list:
-        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection?$skip=%s' % (idrac_ip, seq) ,verify=False,auth=(idrac_username,idrac_password))
+        if args["x"]:
+            response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection?$skip=%s' % (idrac_ip, seq), verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
+        else:
+            response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection?$skip=%s' % (idrac_ip, seq), verify=verify_cert,auth=(idrac_username, idrac_password))
         data = response.json()
-        if response.status_code == 200 or response.status_code == 202:
-            pass
-        else:
+        if response.status_code != 200:
             if "query parameter $skip is out of range" in data["error"]["@Message.ExtendedInfo"][0]["Message"]:
-                f.writelines("\n</CIM>")
-                f.close()
-                print("\n- PASS, iDRAC Server Slot Information captured in \"%s_server_slot_info.xml\" file" % idrac_ip)
-                sys.exit()
+                open_file.writelines("\n</CIM>")
+                open_file.close()
+                logging.info("\n- PASS, iDRAC Server Slot Information captured in \"%s_server_slot_info.xml\" file" % idrac_ip)
+                sys.exit(0)
             else:
-                print("\n- FAIL, GET request failed using skip query parameter, status code %s returned. Detailed error results: \n%s" % (response.status_code,data))    
-                sys.exit()
-        if "Members" in data:
-            pass
-        else:
+                logging.error("\n- FAIL, GET request failed using skip query parameter, status code %s returned. Detailed error results: \n%s" % (response.status_code,data))    
+                sys.exit(0)
+        if "Members" not in data or data["Members"] == [] or response.status_code == 400:
             break
         if data['Members'] == []:
-            print("- FAIL, no data detected for Members property. Manually execute GET on URI 'https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' in browser to check. If no data detected, reboot server and run Collecting Inventory to refresh the configuration database for iDRAC, try GET again." % idrac_ip)
-            sys.exit()
-        else:
-            pass
+            logging.error("- FAIL, no data detected for Members property. Manually execute GET on URI 'https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection' in browser to check. If no data detected, reboot server and run Collecting Inventory to refresh the configuration database for iDRAC, try GET again." % idrac_ip)
+            sys.exit(0)
         for i in data['Members']:
             create_dict = {}
             for ii in i.items():
@@ -161,20 +186,40 @@ def get_server_slot_info_xml():
                 elif ii[0] == "NumberDescription":
                     create_dict["Slot Number"] = str(ii[1])
             create_string = "<VALUE.NAMEDINSTANCE>\n<INSTANCENAME DEVICENAME=\""+create_dict["Id"]+"\">\n<KEYBINDING PROPERTY=\"Slot Number\">\n<VALUE>"+create_dict["Slot Number"]+"</VALUE>\n</KEYBINDING>\n</INSTANCENAME>\n<PROPERTY PROPERTY=\"EmptySlot\">\n<VALUE>"+create_dict["EmptySlot"]+"</VALUE>\n</PROPERTY>\n</VALUE.NAMEDINSTANCE>"  
-            f.writelines(create_string)
-    print("\n- PASS, iDRAC Server Slot Information captured in \"%s_server_slot_info.xml\" file" % idrac_ip)
-    f.writelines("\n</CIM>")
-    f.close()
+            open_file.writelines(create_string)
+    logging.info("\n- INFO, iDRAC Server Slot Information captured in \"%s_server_slot_info.xml\" file" % idrac_ip)
+    open_file.writelines("\n</CIM>")
+    open_file.close()
     
 
-#Run Code
-
 if __name__ == "__main__":
-    if args["s"]:
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] or args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+            verify_cert = False
+        check_supported_idrac_version()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    if args["get_text"]:
         get_server_slot_info()
-    if args["x"]:
+    elif args["get_xml"]:
         get_server_slot_info_xml()
     else:
-        print("\n- FAIL, either missing parameter(s) or invalid paramter value(s) passed in. Refer to help text if needed for supported parameters and values along with script examples")
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
 
 
