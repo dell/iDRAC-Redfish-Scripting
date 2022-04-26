87a4603057ae7b20b91ea8667fcdbd9a28aa2e17
texroemer <30351677+texroemer@users.noreply.github.com>
2022-04-26 08:56:49 -0700

    Multiple updates
    - Updated code to be PEP compliant
    - Updated script examples 
    - Updated argument naming, easier to understand
    - Added x-auth token support to run redfish calls
    - Argument -p not passed in, script will prompt to enter user password, not echo to the screen
    - Added optional argument to check SSL cert for all Redfish calls. Default behavior is to not check for any Redfish calls

--- Redfish Python/ExportLCLogREDFISH.py
+++ Redfish Python/ExportLCLogREDFISH.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
-#
-# ExportLCLogREDFISH. Python script using Redfish API with OEM extension to export iDRAC lifecycle (LC) logs to either local directory or a network share
+#!/usr/bin/python
+#!/usr/bin/python3
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 2.0
+# _version_ = 5.0
 #
 # Copyright (c) 2019, Dell, Inc.
 #
@@ -16,84 +15,73 @@
 #
 
 
-import json
-import sys
-import warnings
 import argparse
-import os
-from datetime import datetime
+import getpass
+import json
+import logging
+import re
 import requests
+import sys
+import time
+import warnings
+import webbrowser
 
+from datetime import datetime
+from pprint import pprint
 
 warnings.filterwarnings("ignore")
 
 parser=argparse.ArgumentParser(description="Python script using Redfish API with OEM extension to export lifecycle (LC) logs to either local directory or network share")
-parser.add_argument('-ip',help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-parser.add_argument('script_examples',action="store_true",help='ExportLCLogREDFISH.py -ip 192.168.0.120 -u root -p calvin --ipaddress 192.168.0.130 --sharetype CIFS --sharename cifs_share_vm --username administrator --password pass --filename idrac_lc_logs.xml, this example will export iDRAC LC logs to a CIFS share. ExportLCLogREDFISH.py -ip 192.168.0.120 -u root -p calvin --sharetype local, this example will export the iDRAC Lifecycle Logs to local directory in XML file format.')
-parser.add_argument('-s', help='Get supported network share types, pass in \"y\"', required=False)
-parser.add_argument('--ipaddress', help='Pass in the IP address of the network share', required=False)
-parser.add_argument('--sharetype', help='Pass in the share type of the network share to export or pass in "Local" to perform local export. For supported network share types, execute -s argument. NOTE: When passing in Local for share type, you don\'t need to pass in any other arguments. This exported LC log file will be in XML format.', required=False)
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password. If you do not pass in argument -p, script will prompt to enter user password which will not be echoed to the screen.', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in value \"true\" or \"false\". By default, this argument is not required and script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', action="store_true", help='Prints script examples')
+parser.add_argument('--shareip', help='Pass in the IP address of the network share', required=False)
+parser.add_argument('--sharetype', help='Pass in the share type of the network share. Supported values: Local, NFS, CIFS, HTTP and HTTPS.', required=False)
 parser.add_argument('--sharename', help='Pass in the network share name', required=False)
-parser.add_argument('--username', help='Pass in the network share username. This argument is only required for CIFS share', required=False)
-parser.add_argument('--password', help='Pass in the network share username password. This argument is only required for CIFS share', required=False)
+parser.add_argument('--username', help='Pass in the network share username if your share is setup for auth.', required=False)
+parser.add_argument('--password', help='Pass in the network share username password if your share is setup for auth', required=False)
 parser.add_argument('--workgroup', help='Pass in the workgroup of your CIFS network share. This argument is optional', required=False)
-parser.add_argument('--filename', help='Pass in unique file name string for the export LC log file', required=False)
-parser.add_argument('--ignorecertwarning', help='Supported values are Off and On. This argument is only required if using HTTPS for share type', required=False)
+parser.add_argument('--filename', help='Pass in unique filename for export LC log file which will get created on the network share. File details will be exported in XML format. Note: This argument is only required for exporting to network share.', required=False)
+parser.add_argument('--ignorecertwarning', help='Supported values are Enabled and Disabled. This argument is only required if using HTTPS for share type', required=False)
 
 args=vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
-idrac_ip=args["ip"]
-idrac_username=args["u"]
-idrac_password=args["p"]
+def script_examples():
+    print("""\n- ExportLCLogREDFISH.py -ip 192.168.0.120 -u root -p calvin --shareip 192.168.0.130 --sharetype CIFS --sharename cifs_share_vm --username administrator --password pass --filename idrac_lc_logs.xml, this example will export iDRAC LC logs to a CIFS share.
+    \n- ExportLCLogREDFISH.py -ip 192.168.0.120 -u root -p calvin --sharetype local, this example will export the iDRAC Lifecycle Logs to local directory in XML file format.""")
+    sys.exit(0)
+
+    
 
 
 def check_supported_idrac_version():
-    response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    if response.__dict__['reason'] == "Unauthorized":
-        print("\n- FAIL, unauthorized to execute Redfish command. Check to make sure you are passing in correct iDRAC username/password and the IDRAC user has the correct privileges")
-        sys.exit(1)
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
     else:
-        pass
+        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
     data = response.json()
-    supported = "no"
-    for i in data['Actions'].keys():
-        if "ExportLCLog" in i:
-            supported = "yes"
-        else:
-            pass
-    if supported == "no":
-        print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
-        sys.exit(1)
-    else:
-        pass
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned, check your iDRAC username/password is correct or iDRAC user has correct privileges to execute Redfish commands" % response.status_code)
+        sys.exit(0)
+    if response.status_code != 200:
+        logging.warning("\n- WARNING, GET command failed to check supported iDRAC version, status code %s returned" % response.status_code)
+        sys.exit(0)
 
-
-def get_supported_network_share_types():
-    response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    data = response.json()
-    print("\n- Supported network share types for ExportLCLog Action -\n")
-    for i in data['Actions'].items():
-        if i[0] == "#DellLCService.ExportLCLog":
-            for ii in i[1]['ShareType@Redfish.AllowableValues']:
-                print(ii)
-
-
-def export_lc_log():
+def export_lc_logs():
     global job_id
-    url = 'https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService/Actions/DellLCService.ExportLCLog' % (idrac_ip)
+    url = 'https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService/Actions/DellLCService.ExportLCLog' % idrac_ip
     method = "ExportLCLog"
-    headers = {'content-type': 'application/json'}
-    payload={}
-
-    headers = {'content-type': 'application/json'}
-    payload={}
-    if args["ipaddress"]:
-        payload["IPAddress"] = args["ipaddress"]
+    payload = {}
+    if args["shareip"]:
+        payload["IPAddress"] = args["shareip"]
     if args["sharetype"]:
-        if args["sharetype"] == "local" or args["sharetype"] == "Local":
+        if args["sharetype"].lower() == "local":
             payload["ShareType"] = args["sharetype"].title()
+            logging.info("\n- INFO, collecting LC log data, this may take 15-30 seconds depending on LC log file size")
         else:
             payload["ShareType"] = args["sharetype"].upper()
     if args["sharename"]:
@@ -108,88 +96,111 @@ def export_lc_log():
         payload["Workgroup"] = args["workgroup"]
     if args["ignorecertwarning"]:
         payload["IgnoreCertWarning"] = args["ignorecertwarning"]
-    print("\n- WARNING, arguments and values for %s method\n" % method)
-    for i in payload.items():
-        print("%s: %s" % (i[0],i[1]))
-    response = requests.post(url, data=json.dumps(payload), headers=headers, verify=False,auth=(idrac_username,idrac_password))
-    data = response.json()
-    if response.status_code == 202:
-        print("\n- PASS: POST command passed for %s method, status code 202 returned" % method)
+    if args["x"]:
+        headers = {'content-type': 'application/json', 'X-Auth-Token': args["x"]}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert)
     else:
-        print("\n- FAIL, POST command failed for %s method, status code is %s" % (method, response.status_code))
+        headers = {'content-type': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert,auth=(idrac_username,idrac_password))
+    if response.status_code == 202:
+        logging.info("\n- PASS: POST command passed for %s method, status code 202 returned" % method)
+    else:
+        logging.error("\n- FAIL, POST command failed for %s method, status code is %s" % (method, response.status_code))
         data = response.json()
-        print("\n- POST command failure results:\n %s" % data)
-        sys.exit(1)
-    if args["sharetype"] == "local" or args["sharetype"] == "Local":
-        get_service_tag = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1' % (idrac_ip),verify=False,auth=(idrac_username, idrac_password))
-        data_get_service_tag = get_service_tag.json()
-        chassis_service_tag = data_get_service_tag['Oem']['Dell']['DellSystem']['NodeID']
-        print("- WARNING, Redfish export LC log URI: \"%s\"\n" % response.headers['Location'])
-        response = requests.get('https://%s%s' % (idrac_ip, response.headers['Location']),verify=False,auth=(idrac_username, idrac_password))
-        get_datetime_info = datetime.now()
-        if args["filename"]:
-            export_filename = args["filename"]
+        logging.error("\n- POST command failure results:")
+        logging.error(data)
+        sys.exit(0)
+    if args["sharetype"].lower() == "local":
+        if response.headers['Location'] == "/redfish/v1/Dell/lclog.xml":
+            logging.info("- INFO, export LC log filename location: \"%s\"" % response.headers['Location'])
+            python_version = sys.version_info
+            while True:
+                if python_version.major <= 2:
+                    request = raw_input("\n* Would you like to open browser session to download exported LC log file? Type \"y\" to download or \"n\" to not download: ")
+                elif python_version.major >= 3:
+                    request = input("\n* Would you like to open browser session to download exported LC log file? Type \"y\" to download or \"n\" to not download: ")
+                else:
+                    logging.error("- FAIL, unable to get current python version, manually run GET on URI \"%s\" to download exported LC log file" % response.headers['Location'])
+                    sys.exit(0)
+                if str(request).lower() == "y":
+                    webbrowser.open('https://%s%s' % (idrac_ip, response.headers['Location']))
+                    logging.info("\n- INFO, check you default browser session for downloaded exported LC log file")
+                    sys.exit(0)
+                elif str(request).lower() == "n":
+                    sys.exit(0)
+                else:
+                    logging.error("- FAIL, incorrect value passed in for request, try again")
+                    continue
         else:
-            export_filename = "%s-%s-%s_%s%s%s_export_LC_log_%s.xml"% (get_datetime_info.year, get_datetime_info.month, get_datetime_info.day, get_datetime_info.hour, get_datetime_info.minute, get_datetime_info.second, chassis_service_tag)
-        filename_open = open(export_filename, "a")
-        dict_response = response.__dict__['_content']
-        string_convert=str(dict_response)
-        string_convert=string_convert.lstrip("'b")
-        string_convert=string_convert.rstrip("'")
-        string_convert=string_convert.split("\\n")
-        for i in string_convert:
-            filename_open.writelines(i)
-            filename_open.writelines("\n")
-        filename_open.close()
-        print("- Exported LC log captured to file \"%s\\%s\"" % (os.getcwd(), export_filename))
-        sys.exit()
+            logging.error("- ERROR, unable to locate exported LC log URI in headers output")
+            sys.exit(0)
     else:
         try:
             job_id = response.headers['Location'].split("/")[-1]
         except:
-            print("- FAIL, unable to find job ID in headers POST response, headers output is:\n%s" % response.headers)
-            sys.exit(1)
-        print("- PASS, job ID %s successfuly created for %s method\n" % (job_id, method))
-
+            logging.error("- FAIL, unable to find job ID in headers POST response, headers output is:\n%s" % response.headers)
+            sys.exit(0)
+        logging.info("- PASS, job ID %s successfuly created for %s method\n" % (job_id, method))
 
 def loop_job_status():
-    start_time=datetime.now()
+    start_time = datetime.now()
     while True:
-        req = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Jobs/%s' % (idrac_ip, job_id), auth=(idrac_username, idrac_password), verify=False)
-        current_time=(datetime.now()-start_time)
-        statusCode = req.status_code
-        if statusCode == 200:
-            pass
+        if args["x"]:
+            response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Jobs/%s' % (idrac_ip, job_id), verify=verify_cert, headers={'X-Auth-Token': args["x"]})
         else:
-            print("\n- FAIL, Command failed to check job status, return code is %s" % statusCode)
-            print("Extended Info Message: {0}".format(req.json()))
-            sys.exit(1)
-        data = req.json()
+            response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Jobs/%s' % (idrac_ip, job_id), verify=verify_cert,auth=(idrac_username, idrac_password))
+        current_time = (datetime.now() - start_time)
+        if response.status_code != 200:
+            logging.error("\n- FAIL, GET command failed to check job status, return code %s" % response.status_code)
+            logging.error("Extended Info Message: {0}".format(response.json()))
+            sys.exit(0)
+        data = response.json()
         if str(current_time)[0:7] >= "0:05:00":
-            print("\n- FAIL: Timeout of 5 minutes has been hit, script stopped\n")
-            sys.exit(1)
-        elif "Fail" in data[u'Message'] or "fail" in data[u'Message'] or data[u'JobState'] == "Failed" or "Unable" in data[u'Message']:
-            print("- FAIL: job ID %s failed, failed message is: %s" % (job_id, data[u'Message']))
-            sys.exit(1)
-        elif data[u'JobState'] == "Completed":
-            if data[u'Message'] == "LCL Export was successful":
-                print("\n--- PASS, Final Detailed Job Status Results ---\n")
+            logging.error("\n- FAIL: Timeout of 5 minutes has been hit, script stopped\n")
+            sys.exit(0)
+        elif "Fail" in data['Message'] or "fail" in data['Message'] or data['JobState'] == "Failed" or "Unable" in data['Message']:
+            logging.error("- FAIL: job ID %s failed, failed message is: %s" % (job_id, data['Message']))
+            sys.exit(0)
+        elif data['JobState'] == "Completed":
+            if data['Message'] == "LCL Export was successful":
+                logging.info("\n--- PASS, Final Detailed Job Status Results ---\n")
             else:
-                print("\n--- FAIL, Final Detailed Job Status Results ---\n")
+                logging.error("\n--- FAIL, Final Detailed Job Status Results ---\n")
             for i in data.items():
-                if "odata" in i[0] or "MessageArgs" in i[0] or "TargetSettingsURI" in i[0]:
-                    pass
-                else:
-                    print("%s: %s" % (i[0],i[1]))
+                pprint(i)
             break
         else:
-            print("- WARNING, job state not marked completed, current job status is running, polling again")
+            logging.info("- INFO, job state not marked completed, current job status is running, polling again")
+            time.sleep(2)
 
 
 if __name__ == "__main__":
-    check_supported_idrac_version()
-    if args["s"]:
-        get_supported_network_share_types()
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] or args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+            verify_cert = False
+        check_supported_idrac_version()
     else:
-        export_lc_log()
-        loop_job_status()
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    export_lc_logs()
+    loop_job_status()
+    
+        
+            
+        
+        
