ebe692833cb5cb1a359d24bdfbc9aadaf9885af4
texroemer <30351677+texroemer@users.noreply.github.com>
2022-04-26 12:11:55 -0700

    Multiple updates
    - Updated code to be PEP compliant
    - Updated script examples 
    - Updated argument naming, easier to understand
    - Added x-auth token support to run redfish calls
    - Argument -p not passed in, script will prompt to enter user password, not echo to the screen
    - Added optional argument to check SSL cert for all Redfish calls. Default behavior is to not check for any Redfish calls

--- Redfish Python/ExportServerScreenShotREDFISH.py
+++ Redfish Python/ExportServerScreenShotREDFISH.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+#!/usr/bin/python3
 #
 # ExportServerScreenShotOemREDFISH. Python script using Redfish API with OEM extension to export current server screen shot
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 1.0
+# _version_ = 2.0
 #
 # Copyright (c) 2020, Dell, Inc.
 #
@@ -14,86 +16,83 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
 
-
-import requests, json, sys, re, time, warnings, argparse, os
+import argparse
+import json
+import logging
+import requests
+import os
+import sys
+import warnings
 
 from datetime import datetime
 
 warnings.filterwarnings("ignore")
 
 parser=argparse.ArgumentParser(description="Python script using Redfish API with OEM extension to export either last crash screen or server screen shot. NOTE: This image will be exported in base64 format to a file. You will need to take this content and use a utility which can convert base64 to PNG.")
-parser.add_argument('-ip',help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-parser.add_argument('script_examples',action="store_true",help='ExportServerScreenShotOemREDFISH.py -ip 192.168.0.120 -u root -p calvin -f 2, this example will export a screenshot of the current server status.')
-parser.add_argument('-f', help='Pass in the filetype to export. Either \"0\" for LastCrashScreenShot, \"1\" for Preview or \"2\" for ServerScreenShot', required=False)
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password. If you do not pass in argument -p, script will prompt to enter user password which will not be echoed to the screen.', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in value \"true\" or \"false\". By default, this argument is not required and script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', action="store_true", help='Prints script examples')
+parser.add_argument('--filetype', help='Pass in the filetype to export. Supported values: 0, 1 and 2. 0 for LastCrashScreenShot, 1 for Preview or 2 for ServerScreenShot', required=False)
 
-args=vars(parser.parse_args())
+args = vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
-idrac_ip=args["ip"]
-idrac_username=args["u"]
-idrac_password=args["p"]
-
-try:
-    os.remove("export_screenshot.txt")
-except:
-    pass
-
+def script_examples():
+    print("""\n- ExportServerScreenShotOemREDFISH.py -ip 192.168.0.120 -u root -p calvin --filetype 2, this example will export a screenshot of the current server status.
+    \n- ExportServerScreenShotREDFISH.py -ip 192.168.0.120 -x 778e341f432fbd0319ff8ecdc5379a12 --filetype 0, this example using X-auth token session will export last server screenshot crash.""")
+    sys.exit(0)
 
 def check_supported_idrac_version():
-    response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    if response.__dict__['reason'] == "Unauthorized":
-        print("\n- FAIL, unauthorized to execute Redfish command. Check to make sure you are passing in correct iDRAC username/password and the IDRAC user has the correct privileges")
-        sys.exit()
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
     else:
-        pass
+        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
     data = response.json()
-    supported = "no"
-    for i in data['Actions'].keys():
-        if "ExportServerScreenShot" in i:
-            supported = "yes"
-        else:
-            pass
-    if supported == "no":
-        print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
-        sys.exit()
-    else:
-        pass
-
-
-
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned, check your iDRAC username/password is correct or iDRAC user has correct privileges to execute Redfish commands" % response.status_code)
+        sys.exit(0)
+    if response.status_code != 200:
+        logging.warning("\n- WARNING, GET command failed to check supported iDRAC version, status code %s returned" % response.status_code)
+        sys.exit(0)
 
 def export_server_screen_shot():
     global job_id
     url = 'https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService/Actions/DellLCService.ExportServerScreenShot' % (idrac_ip)
     method = "ExportServerScreenShot"
-    headers = {'content-type': 'application/json'}
     payload={}
-    if args["f"] == "0":
+    if args["filetype"] == "0":
         payload["FileType"] = "LastCrashScreenShot"
-    elif args["f"] == "1":
+    elif args["filetype"] == "1":
         payload["FileType"] = "Preview"
-    elif args["f"] == "2":
+    elif args["filetype"] == "2":
         payload["FileType"] = "ServerScreenShot"
     else:
-        print("- FAIL, invalid value passed in for argument -f")
+        logging.error("- FAIL, invalid value passed in for argument --filetype")
         sys.exit()
-    print("\n- WARNING, arguments and values for %s method\n" % method)
-    for i in payload.items():
-          print("%s: %s" % (i[0],i[1]))
-    response = requests.post(url, data=json.dumps(payload), headers=headers, verify=False,auth=(idrac_username,idrac_password))
-    data = response.json()
-    print(data.keys())
-    if response.status_code == 202 or response.status_code == 200:
-        print("\n- PASS: POST command passed for %s method, status code %s returned" % (method, response.status_code))
+    if args["x"]:
+        headers = {'content-type': 'application/json', 'X-Auth-Token': args["x"]}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert)
     else:
-        print("\n- FAIL, POST command failed for %s method, status code is %s" % (method, response.status_code))
+        headers = {'content-type': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert,auth=(idrac_username,idrac_password))
+    data = response.json()
+    if response.status_code == 202 or response.status_code == 200:
+        try:
+            os.remove("export_screenshot.txt")
+        except:
+            logging.info("- INFO, unable to remove %s file, file does not exist in current directory" % "export_screenshot.txt")
+        logging.info("\n- PASS: POST command passed for %s method, status code %s returned" % (method, response.status_code))
+    else:
+        logging.error("\n- FAIL, POST command failed for %s method, status code is %s" % (method, response.status_code))
         data = response.json()
-        print("\n- POST command failure results:\n %s" % data)
-        sys.exit()
-    with open("export_screenshot.txt","w") as x:
-        x.writelines(data['ServerScreenShotFile'])
-    print("\n- PASS, screenshot exported locally to file \"export_screenshot.txt\". Take the contents and copy to a utility which can convert base64 into PNG file to view the screenshot")
+        logging.error("\n- POST command failure results:\n %s" % data)
+        sys.exit(0)
+    with open("export_screenshot.txt","w") as open_file:
+        open_file.writelines(data['ServerScreenShotFile'])
+    logging.info("\n- PASS, screenshot exported locally to file \"export_screenshot.txt\". Take the contents and copy to a utility which can convert base64 into PNG file to view the screenshot")
     
     
 
@@ -104,11 +103,32 @@ def export_server_screen_shot():
     
 
 if __name__ == "__main__":
-    if args["f"]:
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] or args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+            verify_cert = False
         check_supported_idrac_version()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    if args["filetype"]:
         export_server_screen_shot()
     else:
-        print("\n- FAIL, either missing parameter(s) or incorrect parameter(s) passed in. If needed, execute script with -h for script help")
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
     
     
         
