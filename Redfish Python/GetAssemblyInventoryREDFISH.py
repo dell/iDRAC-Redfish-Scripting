900d6911a7d185897a9280d231cc7b836becca80
texroemer <30351677+texroemer@users.noreply.github.com>
2022-04-27 15:13:33 -0700

    Multiple updates
    - Updated code to be PEP compliant
    - Updated script examples 
    - Updated argument naming, easier to understand
    - Added x-auth token support to run redfish calls
    - Argument -p not passed in, script will prompt to enter user password, not echo to the screen
    - Added optional argument to check SSL cert for all Redfish calls. Default behavior is to not check for any Redfish calls

--- Redfish Python/GetAssemblyInventoryREDFISH.py
+++ Redfish Python/GetAssemblyInventoryREDFISH.py
@@ -2,7 +2,7 @@
 # GetAssemblyInventoryREDFISH. Python script using Redfish API DMTF to get system assembly or hardware inventory
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 1.0
+# _version_ = 2.0
 #
 # Copyright (c) 2019, Dell, Inc.
 #
@@ -15,123 +15,109 @@
 #
 
 
-import requests, json, sys, re, time, warnings, argparse, os
+import argparse
+import getpass
+import json
+import logging
+import os
+import pickle
+import re
+import requests
+import sys
+import time
+import warnings
 
 from datetime import datetime
+from pprint import pprint
 
 warnings.filterwarnings("ignore")
 
 parser=argparse.ArgumentParser(description="Python script using Redfish API DMTF to get system assembly (hardware) inventory(output will be printed to the screen and also copied to a text file). This includes information for storage controllers, memory, network devices, motherboard(planar), power supplies, backplanes")
-parser.add_argument('-ip',help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-parser.add_argument('script_examples',action="store_true",help='GetAssemblyInventoryREDFISH.py -ip 192.168.0.120 -u root -p calvin -a y, this example will get all chassis assembly URIs. GetAssemblyInventoryREDFISH.py -ip 192.168.0.120 -u root -p calvin -A y, this example will get detailed information for all chassis assembly URIs. GetAssemblyInventoryREDFISH.py -ip 192.168.0.120 -u root -p calvin -s /redfish/v1/Chassis/System.Embedded.1/Assembly/DIMM.Socket.A2, this example will only return data for this URI')
-parser.add_argument('-a', help='Get device chassis assembly URIs, pass in \"y\"', required=False)
-parser.add_argument('-A', help='Get detailed information for all chassis assembly URIs, pass in \"y\"', required=False)
-parser.add_argument('-s', help='Get detailed information for only a specific chassis assembly URI, pass in the URI', required=False)
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password. If you do not pass in argument -p, script will prompt to enter user password which will not be echoed to the screen.', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in value \"true\" or \"false\". By default, this argument is not required and script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', action="store_true", help='Prints script examples')
+parser.add_argument('--get-details-all', help='Get detailed information for all chassis assembly URIs', action="store_true", dest="get_details_all", required=False)
+parser.add_argument('--get-details-uri', help='Get detailed information for only a specific chassis assembly URI, pass in the URI', dest="get_details_uri", required=False)
 
-args=vars(parser.parse_args())
+args = vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
-idrac_ip=args["ip"]
-idrac_username=args["u"]
-idrac_password=args["p"]
-
-try:
-    os.remove("assembly_inventory.txt")
-except:
-    pass
-
-
-
-f=open("assembly_inventory.txt","a")
-d=datetime.now()
-current_date_time="- Data collection timestamp: %s-%s-%s  %s:%s:%s\n" % (d.month,d.day,d.year, d.hour,d.minute,d.second)
-f.writelines(current_date_time)
-f.close()
+def script_examples():
+    print("""\n- GetAssemblyInventoryREDFISH.py -ip 192.168.0.120 -u root -p calvin --get-details-all, this example will get detailed information for all chassis assembly URIs.
+    \n- GetAssemblyInventoryREDFISH.py -ip 192.168.0.120 -u root -p calvin --get-details-uri /redfish/v1/Chassis/System.Embedded.1/Assembly/DIMM.Socket.A2, this example will only return data for this URI""")
+    sys.exit(0)
 
 def check_supported_idrac_version():
-    response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1/Assembly' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1/Assembly' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1/Assembly' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned, check your iDRAC username/password is correct or iDRAC user has correct privileges to execute Redfish commands" % response.status_code)
+        sys.exit(0)
+    if response.status_code != 200:
+        logging.warning("\n- WARNING, GET command failed to check supported iDRAC version, status code %s returned" % response.status_code)
+        sys.exit(0)
+
+def get_details_all():
+    logging.info("\n- INFO, chassis assembly details for iDRAC %s - \n" % idrac_ip)
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1/Assembly' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1/Assembly' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
     data = response.json()
     if response.status_code != 200:
-        print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
-        sys.exit()
-    else:
-        pass
-
-
-def get_assembly_uris():
-    response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1/Assembly' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    data = response.json()
-    if response.status_code != 200:
-        print("\n- FAIL, get command failed, error is: %s" % data)
-        sys.exit()
-    else:
-        pass
-    assembly_uris = []
-    print("\n- Chassis Assembly URIs -\n")
-    for i in data[u'Assemblies']:
-        for ii in i.items():
-            if ii[0] == u'@odata.id':
-                print(ii[1])
-                assembly_uris.append(ii[1])
-    if args["A"]:
-        f=open("assembly_inventory.txt","a")
-        for i in assembly_uris:
-            message = "\n- Detailed information for URI %s -\n" % i
-            f.writelines(message)
-            f.writelines("\n")
-            print(message)
-            response = requests.get('https://%s%s' % (idrac_ip, i),verify=False,auth=(idrac_username, idrac_password))
-            data = response.json()
-            for ii in data.items():
-                if ii[0] == u'@odata.id' or ii[0] == u'@odata.context' or ii[0] == u'Metrics' or ii[0] == u'Links' or ii[0] ==  u'@odata.type':
-                    pass
-                else:
-                    message = "%s: %s" % (ii[0], ii[1])
-                    f.writelines(message)
-                    f.writelines("\n")
-                    print(message)
-        print("\n- WARNING, output also captured in \"%s\\assembly_inventory.txt\" file" % os.getcwd())
-    else:
-        pass
-            
-    
-    
-    
+        logging.error("\n- FAIL, get command failed, error: %s" % data)
+        sys.exit(0)
+    for i in data.items():
+        pprint(i)
 
 def get_specific_uri_info():
-    response = requests.get('https://%s%s' % (idrac_ip, args["s"]),verify=False,auth=(idrac_username, idrac_password))
+    if args["x"]:
+        response = requests.get('https://%s%s' % (idrac_ip, args["get_details_uri"]), verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s%s' % (idrac_ip, args["get_details_uri"]), verify=verify_cert, auth=(idrac_username, idrac_password))
     data = response.json()
     if response.status_code != 200:
-        print("\n- FAIL, get command failed, error is: %s" % data)
-        sys.exit()
-    else:
-        pass
-    print("\n- Detailed information for URI %s -\n" % args["s"])
-    for ii in data.items():
-        if ii[0] == u'@odata.id' or ii[0] == u'@odata.context' or ii[0] == u'Metrics' or ii[0] == u'Links' or ii[0] ==  u'@odata.type':
-            pass
-        else:
-            print("%s: %s" % (ii[0], ii[1]))
-                    
+        logging.error("\n- FAIL, get command failed, error is: %s" % data)
+        sys.exit(0)
+    logging.info("\n- Detailed information for URI %s -\n" % args["get_details_uri"])
+    for i in data.items():
+        pprint(i)
        
 if __name__ == "__main__":
-    check_supported_idrac_version()
-    if args["a"] or args["A"]:
-          get_assembly_uris()
-    elif args["s"]:
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] or args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+            verify_cert = False
+        check_supported_idrac_version()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    if args["get_details_all"]:
+          get_details_all()
+    elif args["get_details_uri"]:
         get_specific_uri_info()
     else:
-        print("\n- FAIL, invalid argument passed in or missing required argument")
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
         
-        
-        
-    
-
-    
-    
-    
-        
-            
-        
+         
         
