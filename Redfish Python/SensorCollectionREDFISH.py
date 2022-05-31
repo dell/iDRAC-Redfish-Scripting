4f38ed22bb997c69e55d7bb88e26d0b0a71ad453
texroemer <30351677+texroemer@users.noreply.github.com>
2022-05-31 14:18:20 -0700

    Multiple updates
    - Updated code to be PEP compliant
    - Updated script examples 
    - Updated argument naming, easier to understand
    - Added x-auth token support to run redfish calls
    - Argument -p not passed in, script will prompt to enter user password, not echo to the screen
    - Added optional argument to check SSL cert for all Redfish calls. Default behavior is to not check for any Redfish calls

--- Redfish Python/SensorCollectionREDFISH.py
+++ Redfish Python/SensorCollectionREDFISH.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+#!/usr/bin/python3
 #
 # AggregationMetricValueCollectionREDFISH. Python script using Redfish API OEM extensoion to get iDRAC sensor collection data.
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 1.0
+# _version_ = 2.0
 #
 # Copyright (c) 2019, Dell, Inc.
 #
@@ -14,99 +16,148 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 #
 
-import requests, json, sys, re, time, os, warnings, argparse
+import argparse
+import getpass
+import json
+import logging
+import re
+import requests
+import sys
+import time
+import warnings
 
 from datetime import datetime
+from pprint import pprint
 
 warnings.filterwarnings("ignore")
 
 parser=argparse.ArgumentParser(description="Python script using Redfish API OEM extension to sensor collection data.")
-parser.add_argument('-ip',help='iDRAC IP address', required=True)
-parser.add_argument('-u', help='iDRAC username', required=True)
-parser.add_argument('-p', help='iDRAC password', required=True)
-parser.add_argument('-n', help='Get all Dell Numeric Sensor Collection data, pass in \"y\"', required=False)
-parser.add_argument('-ps', help='Get all Dell PS(power supply) Numeric Sensor Collection data, pass in \"y\"', required=False)
-parser.add_argument('-pss', help='Get all Dell Presence And Status Sensor Collection data, pass in \"y\"', required=False)
-parser.add_argument('-s', help='Get all Dell Sensor Collection data, pass in \"y\"', required=False)
-args=vars(parser.parse_args())
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password. If you do not pass in argument -p, script will prompt to enter user password which will not be echoed to the screen.', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in value \"true\" or \"false\". By default, this argument is not required and script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', help='Get executing script examples', action="store_true", dest="script_examples", required=False) 
+parser.add_argument('--get-numeric', help='Get all Dell Numeric Sensor Collection data', action="store_true", required=False)
+parser.add_argument('--get-power', help='Get all Dell PS(power supply) Numeric Sensor Collection data', action="store_true", required=False)
+parser.add_argument('--get-status', help='Get all Dell Presence And Status Sensor Collection data', action="store_true", required=False)
+parser.add_argument('--get-sensor', help='Get all Dell Sensor Collection data', action="store_true", required=False)
+args = vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
-idrac_ip=args["ip"]
-idrac_username=args["u"]
-idrac_password=args["p"]
-
-try:
-    os.remove("sensor_collection.txt")
-except:
-    pass
+def script_examples():
+    print("""\n- SensorCollectionREDFISH.py -ip 192.168.0.120 -u root -p calvin --get-numeric, this example will return Dell numeric sensor data information.
+    \n- SensorCollectionREDFISH.py -ip 192.168.0.120 -u root -p calvin --get-sensor, this example will return Dell sensor data information.""")
+    sys.exit(0)
 
 def check_supported_idrac_version():
-    response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellNumericSensorCollection' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
-    data = response.json()
-    if response.status_code != 200:
-        print("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
-        sys.exit()
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellNumericSensorCollection' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
     else:
-        pass
-
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellNumericSensorCollection' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned. Incorrect iDRAC username/password or invalid privilege detected." % response.status_code)
+        sys.exit(0)
+    elif response.status_code != 200:
+        logging.warning("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        sys.exit(0)
+    try:
+        os.remove("sensor_collection.txt")
+    except:
+        logging.debug("- INFO, file not detected, skipping step to delete")
 
 def get_sensor_data():
-    f=open("sensor_collection.txt","a")
-    d=datetime.now()
-    current_date_time="- Data collection timestamp: %s-%s-%s  %s:%s:%s\n" % (d.month,d.day,d.year, d.hour,d.minute,d.second)
-    f.writelines(current_date_time)
-    f.writelines("\n\n")
-    if args["n"]:
+    open_file = open("sensor_collection.txt","a")
+    get_time = datetime.now()
+    current_date_time = "- Data collection timestamp: %s-%s-%s  %s:%s:%s\n" % (get_time.month, get_time.day, get_time.year, get_time.hour, get_time.minute, get_time.second)
+    open_file.writelines(current_date_time)
+    open_file.writelines("\n\n")
+    if args["get_numeric"]:
         sensor_key = "DellNumericSensorCollection"
-    elif args["ps"]:
+    elif args["get_power"]:
         sensor_key = "DellPSNumericSensorCollection"
-    elif args["pss"]:
+    elif args["get_status"]:
         sensor_key = "DellPresenceAndStatusSensorCollection"
-    elif args["s"]:
+    elif args["get_sensor"]:
         sensor_key = "DellSensorCollection"
     else:
-        print("- FAIL, you must pass in at least one parameter to get sensor collection data")
-        sys.exit()
-    response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/%s' % (idrac_ip, sensor_key),verify=False,auth=(idrac_username,idrac_password))
-    print("\n- Data collection data for \"%s\"\n" % sensor_key) 
-    data = response.json()
-    if data[u'Members'] == []:
-        print("- WARNING, no data available for URI \"redfish/v1/Dell/Systems/System.Embedded.1/%s\"" % sensor_key)
-        sys.exit()
+        logging.error("- FAIL, you must pass in at least one parameter to get sensor collection data")
+        sys.exit(0)
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/%s' % (idrac_ip, sensor_key), verify=verify_cert, headers={'X-Auth-Token': args["x"]})
     else:
-        pass
-    for i in data[u'Members']:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/%s' % (idrac_ip, sensor_key), verify=verify_cert, auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code != 200:
+        logging.error("\n- FAIL, GET command failed, status code %s returned" % response.status_code)
+        logging.error(data)
+        sys.exit(0)
+    logging.info("\n- Data collection data for \"%s\"\n" % sensor_key) 
+    if data['Members'] == []:
+        logging.warning("- WARNING, no data available for URI \"redfish/v1/Dell/Systems/System.Embedded.1/%s\"" % sensor_key)
+        sys.exit(0)
+    for i in data['Members']:
         for ii in i.items():
             sensor_entry = ("%s: %s" % (ii[0],ii[1]))
             print(sensor_entry)
-            f.writelines("%s\n" % sensor_entry)
+            open_file.writelines("%s\n" % sensor_entry)
         print("\n")
-        f.writelines("\n")
-    if u'Members@odata.nextLink' in data:
-        number_list=[i for i in range (1,100001) if i % 50 == 0]
+        open_file.writelines("\n")
+    if 'Members@odata.nextLink' in data:
+        number_list = [i for i in range (1,100001) if i % 50 == 0]
         for seq in number_list:
-            response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/%s?$skip=%s' % (idrac_ip, sensor_key, seq) ,verify=False,auth=(idrac_username,idrac_password))
-            data = response.json()
-            if "Members" in data:
-                pass
+            if args["x"]:
+                response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection?$skip=%s' % (idrac_ip, seq), verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
             else:
+                response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellSlotCollection?$skip=%s' % (idrac_ip, seq), verify=verify_cert,auth=(idrac_username, idrac_password))
+            data = response.json()
+            if response.status_code != 200:
+                if "query parameter $skip is out of range" in data["error"]["@Message.ExtendedInfo"][0]["Message"]:
+                    open_file.close()
+                    logging.info("\n- INFO, iDRAC Server Slot Information also captured in \"%s_server_slot_info.txt\" file" % idrac_ip)
+                    sys.exit(0)
+                else:
+                    logging.error("\n- FAIL, GET request failed using skip query parameter, status code %s returned. Detailed error results: \n%s" % (response.status_code,data))    
+                    sys.exit(0)
+            if "Members" not in data or data["Members"] == [] or response.status_code == 400:
                 break
-            for i in data[u'Members']:
+            for i in data['Members']:
+                pprint(i), print("\n")
                 for ii in i.items():
-                    lc_log_entry = ("%s: %s" % (ii[0],ii[1]))
-                    print(lc_log_entry)
-                    f.writelines("%s\n" % lc_log_entry)
-                print("\n")
-                f.writelines("\n")   
-    print("\n- WARNING, \"%s\" data also captured in \"sensor_collection.txt\" file" % sensor_key)
-    f.close()
-
+                    server_slot_entry = ("%s: %s" % (ii[0],ii[1]))
+                    open_file.writelines("%s\n" % server_slot_entry)
+                open_file.writelines("\n")
+    logging.info("\n- INFO, \"%s\" data also captured in \"sensor_collection.txt\" file" % sensor_key)
+    open_file.close()
+        
 
 if __name__ == "__main__":
-    check_supported_idrac_version()
-    if args["n"] or args["p"] or args["pss"] or args["s"]:
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] or args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+            verify_cert = False
+        check_supported_idrac_version()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    if args["get_numeric"] or args["get_power"] or args["get_status"] or args["get_sensor"]:
         get_sensor_data()
     else:
-        print(" - WARNING, either incorrect parameter value passed in or missing required parameter")
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
     
-
 
