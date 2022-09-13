82cac3ddd86f45bb40f0c726988d6a659db8859f
texroemer <30351677+texroemer@users.noreply.github.com>
2022-09-13 11:44:33 -0500

    Update CreateDeleteIdracUsersREDFISH.py

--- Redfish Python/CreateDeleteIdracUsersREDFISH.py
+++ Redfish Python/CreateDeleteIdracUsersREDFISH.py
@@ -52,7 +52,7 @@ logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 def script_examples():
     print("""\n- CreateDeleteIdracUsersREDFISH..py -ip 192.168.0.120 -u root -p calvin --get, this example will get all iDRAC user account details.
     \n- CreateDeleteIdracUsersREDFISH.py -ip 192.168.0.120 -u root --user-id 3 --new-user tester --privilege-role 1 --enable y, this example will first prompt to enter password for user root. Then prompt to enter new password for user ID 3 and create this user.
-    \n- CreateDeleteIdracUsersREDFISH..py -ip 192.168.0.120 -u root -p calvin --user-id 3 --new-user user3 --new-pwd test123 --privilege-role 2 --enable y, this example will create iDRAC user for id 3, enable and set privileges to operator.
+    \n- CreateDeleteIdracUsersREDFISH.py -ip 192.168.0.120 -u root -p calvin --user-id 3 --new-user user3 --new-pwd test123 --privilege-role 2 --enable y, this example will create iDRAC user for id 3, enable and set privileges to operator.
     \n- CreateDeleteIdracUsersREDFISH.py -ip 192.168.0.120 -u root -p calvin --delete 3, this example will delete iDRAC user id 3.
     \n- CreateDeleteIdracUsersREDFISH.py -ip 100.65.84.70 -x c09c44e17e09372536428a6369bfa1b2 --delete 7, this example shows deleting user id 7 account using X-auth token session.""")
     sys.exit(0)
