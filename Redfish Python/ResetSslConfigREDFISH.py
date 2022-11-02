bbb80564b3c5e8f410f66d11693bde526fef8169
texroemer <30351677+texroemer@users.noreply.github.com>
2022-11-02 08:48:15 -0700

    New script
    New script using Redfish API with OEM extension to restore the web-server certificate to factory defaults. Once this POST action is invoked, iDRAC reboot is needed to apply this reset change.

---
+++ Redfish Python/ResetSslConfigREDFISH.py
@@ -0,0 +1,128 @@
+#!/usr/bin/python
+#!/usr/bin/python3
+#
+# 
+# _author_ = Texas Roemer <Texas_Roemer@Dell.com>
+# _version_ = 1.0
+#
+# Copyright (c) 2022, Dell, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+import argparse
+import getpass
+import json
+import logging
+import re
+import requests
+import sys
+import time
+import warnings
+
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser(description='Python script using Redfish API with OEM extension to restore the web-server certificate to factory defaults. Once this POST action is invoked, iDRAC reboot is needed to apply this reset change.')
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password. If you do not pass in argument -p, script will prompt to enter user password which will not be echoed to the screen.', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in value \"true\" or \"false\". By default, this argument is not required and script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', help='Get executing script examples', action="store_true", dest="script_examples", required=False)
+parser.add_argument('--reset-ssl', help='Reset SSL or web server certificates back to factory default settings', action="store_true", dest="reset_ssl", required=False)
+parser.add_argument('--reboot-idrac', help='Reboot iDRAC to apply the reset cert change', action="store_true", dest="reboot_idrac", required=False)
+
+args = vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
+
+def script_examples():
+    print("""\n- """)
+    sys.exit(0)
+
+def check_supported_idrac_version():
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DelliDRACCardService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DelliDRACCardService' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned. Incorrect iDRAC username/password or invalid privilege detected." % response.status_code)
+        sys.exit(0)
+    elif response.status_code != 200:
+        logging.warning("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        sys.exit(0)
+
+def reset_ssl():
+    url = "https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DelliDRACCardService/Actions/DelliDRACCardService.SSLResetCfg" % (idrac_ip)
+    method = "SSLResetCfg"
+    payload = {}
+    if args["x"]:
+        headers = {'content-type': 'application/json', 'X-Auth-Token': args["x"]}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert)
+    else:
+        headers = {'content-type': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert,auth=(idrac_username,idrac_password))
+    data=response.json()
+    if response.status_code == 200:
+        logging.info("\n- PASS: POST command passed for %s method, status code 200 returned\n" % method)
+    else:
+        logging.error("\n- FAIL, POST command failed for %s method, status code is %s" % (method, response.status_code))
+        data = response.json()
+        logging.error("\n- POST command failure results:\n %s" % data)
+        sys.exit(0)
+
+def reboot_idrac():
+    url = "https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Actions/Manager.Reset/" % idrac_ip
+    payload={"ResetType":"GracefulRestart"}
+    if args["x"]:
+        headers = {'content-type': 'application/json', 'X-Auth-Token': args["x"]}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert)
+    else:
+        headers = {'content-type': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert,auth=(idrac_username,idrac_password))
+    if response.status_code == 204:
+        logging.info("\n- PASS, POST command passed to reset iDRAC, status code %s returned\n" % response.status_code)
+    else:
+        data = response.json()
+        logging.error("\n- FAIL, status code %s returned, detailed error results \n%s" % (response.status_code, data))
+        sys.exit(0)
+    time.sleep(15)
+    logging.info("- INFO, iDRAC will now reboot and be back online within a few minutes")
+    
+
+if __name__ == "__main__":
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] or args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+            verify_cert = False
+        check_supported_idrac_version()
+    if args["reset_ssl"]:
+        reset_ssl()
+        if args["reboot_idrac"]:
+            logging.info("- INFO, iDRAC will now reboot to apply the reset cert changes")
+            reboot_idrac()
+        else:
+            logging.info("- INFO, argument --reboot-idrac not detected. Changes for reset cert will not be applied until iDRAC has been rebooted.")        
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+
