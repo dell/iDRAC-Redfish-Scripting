78837fa2e2b09cbeff413a6b22dba969e97a11ba
texroemer <30351677+texroemer@users.noreply.github.com>
2022-12-15 15:32:33 -0600

    New script
    New script supported for iDRAC 6.10.00 release, perform server virtual a/c power cycle.

---
+++ Redfish Python/ServerVirtualAcPowerCycleREDFISH.py
@@ -0,0 +1,105 @@
+#
+# ServerVirtualAcPowerCycleREDFISH. Python script using Redfish API to virtual a/c power cycle the server.
+#
+#
+# _author_ = Texas Roemer <Texas_Roemer@Dell.com>
+# _version_ = 1.0
+#
+# Copyright (c) 2022, Dell, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+
+import argparse
+import getpass
+import json
+import logging
+import requests
+import sys
+import warnings
+
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser(description="Python script using Redfish API to virtual a/c power cycle the server. You must have iDRAC9 version 6.10.00 or newer to use this script. Note: Known issue after virtual a/c power cycle has been performed, server will remain in OFF state. This issue will be fixed in a future iDRAC release.")
+parser.add_argument('-ip', help='Pass in iDRAC IP address', required=False)
+parser.add_argument('-u', help='Pass in iDRAC username', required=False)
+parser.add_argument('-p', help='Pass in iDRAC password. If not passed in, script will prompt to enter password which will not be echoed to the screen', required=False)
+parser.add_argument('--ssl', help='Verify SSL certificate for all Redfish calls, pass in \"true\". This argument is optional, if you do not pass in this argument, all Redfish calls will ignore SSL cert checks.', required=False)
+parser.add_argument('-x', help='Pass in iDRAC X-auth token session ID to execute all Redfish calls instead of passing in username/password', required=False)
+parser.add_argument('--script-examples', help='Get executing script examples', action="store_true", dest="script_examples", required=False)
+parser.add_argument('--ac-cycle', help='A/C power cycle the server', action="store_true", dest="ac_cycle", required=False)
+
+
+args = vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
+
+def script_examples():
+    print("""\n- ServerVirtualAcPowerCycleREDFISH.py -ip 192.168.0.120 -u root -p calvin --ac-cycle, this example will a/c power cycle the server.""")
+    sys.exit(0)
+
+def check_supported_idrac_version():
+    if args["x"]:
+         response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code 401 detected, check iDRAC username / password credentials")
+        sys.exit(0)
+    elif response.status_code != 200:
+        logging.warning("\n- WARNING, GET request failed to validate iDRAC creds, status code %s returned." % response.status_code)
+        logging.warning(data)
+        sys.exit(0)
+
+def ac_power_cycle():
+    url = 'https://%s/redfish/v1/Chassis/System.Embedded.1/Actions/Chassis.Reset' % idrac_ip
+    payload = {"ResetType": "PowerCycle"}
+    if args["x"]:
+        headers = {'content-type': 'application/json', 'X-Auth-Token': args["x"]}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert)
+    else:
+        headers = {'content-type': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert, auth=(idrac_username, idrac_password))
+    if response.status_code == 204:
+        logging.info("\n- PASS, POST command passed to a/c power cycle the server, status code %s returned" % response.status_code)
+    else:
+        logging.error("\n- FAIL, Command failed, status code %s returned\n" % response.status_code)
+        logging.error(response.json())
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] and args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+                verify_cert = False
+        check_supported_idrac_version()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    if args["ac_cycle"]:
+        ac_power_cycle()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        
+   
