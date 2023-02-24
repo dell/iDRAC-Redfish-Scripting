bcf4bd35a2f28b1ee679c13ef01c12a40206e1c8
texroemer <30351677+texroemer@users.noreply.github.com>
2023-02-24 15:57:15 -0600

    Multiple updates
    - Add argument -x for X-auth support
    - Add argument --ssl for cert verification 
    - Cleaned up code

--- Redfish Python/SetChassisIndicatorLedREDFISH.py
+++ Redfish Python/SetChassisIndicatorLedREDFISH.py
@@ -1,8 +1,6 @@
 #
-# SetChassisIndicatorledREDFISH. Python script using Redfish API to either get current chassis indicator LED state or set chassis indicator LED state.
-#
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
-# _version_ = 2.0
+# _version_ = 3.0
 #
 # Copyright (c) 2018, Dell, Inc.
 #
@@ -15,6 +13,7 @@
 #
 
 import argparse
+import getpass
 import json
 import logging
 import re
@@ -28,25 +27,41 @@ warnings.filterwarnings("ignore")
 parser=argparse.ArgumentParser(description="Python script using Redfish API to either get current chassis indicator LED state or set chassis indicator LED state")
 parser.add_argument('-ip',help='iDRAC IP address', required=False)
 parser.add_argument('-u', help='iDRAC username', required=False)
-parser.add_argument('-p', help='iDRAC password', required=False)
-parser.add_argument('--script-examples', help='Get examples of executing script.', action="store_true", dest="script_examples", required=False)
+parser.add_argument('-p', help='iDRAC password. If you do not pass in argument -p, script will prompt to enter user password which will not be echoed to the screen.', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in value \"true\" or \"false\". By default, this argument is not required and script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', help='Get executing script examples', action="store_true", dest="script_examples", required=False)
 parser.add_argument('--get', help='Get current chassis indicator LED state', action="store_true", required=False)
 parser.add_argument('--set', help='Set chassis indicator LED state, pass in one of the supported values: \"Lit\" or \"Blinking\". These values are case sensitive so pass in the exact string syntax', required=False)
-args=vars(parser.parse_args())
-
+args = vars(parser.parse_args())
 logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
-idrac_ip=args["ip"]
-idrac_username=args["u"]
-idrac_password=args["p"]
+
 
 def script_examples():
-    print("""SetChassisIndicatorLedREDFISH.py -ip 192.168.0.120 -u root -p calvin --get, this example will return current chassis LED state.
+    print("""\nSetChassisIndicatorLedREDFISH.py -ip 192.168.0.120 -u root -p calvin --get, this example will return current chassis LED state.
     \nSetChassisIndicatorLedREDFISH.py -ip 192.168.0.120 -u root -p calvin --set Lit, this example will disable blinking and set chassis LED state to Lit.
     \nSetChassisIndicatorLedREDFISH.py -ip 192.168.0.120 -u root -p calvin --set Blinking, this example will set chassis LED to blink.""")
+    sys.exit(0)
+
+def check_supported_idrac_version():
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned. Incorrect iDRAC username/password or invalid privilege detected." % response.status_code)
+        sys.exit(0)
+    elif response.status_code != 200:
+        logging.warning("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        sys.exit(0)
 
 def get_current_chassis_indicator_LED_state():
-    response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip,verify=False,auth=(idrac_username, idrac_password))
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip,verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
+    else:
+        response = requests.get('https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip,verify=verify_cert,auth=(idrac_username, idrac_password))
     data = response.json()
     if response.status_code == 401:
         logging.warning("\n- WARNING, status code 401 detected, check iDRAC username / password credentials")
@@ -58,9 +73,14 @@ def get_current_chassis_indicator_LED_state():
 
 def set_current_chassis_indicator_LED_state():
     url = 'https://%s/redfish/v1/Chassis/System.Embedded.1' % idrac_ip
-    payload = {'IndicatorLED': args["set"]}
+    payload = {'IndicatorLED': args["set"].title()}
     headers = {'content-type': 'application/json'}
-    response = requests.patch(url, data=json.dumps(payload), headers=headers, verify=False, auth=(idrac_username,idrac_password))
+    if args["x"]:
+        headers = {'content-type': 'application/json', 'X-Auth-Token': args["x"]}
+        response = requests.patch(url, data=json.dumps(payload), headers=headers, verify=verify_cert)
+    else:
+        headers = {'content-type': 'application/json'}
+        response = requests.patch(url, data=json.dumps(payload), headers=headers, verify=verify_cert,auth=(idrac_username,idrac_password))
     if response.status_code == 200:
         logging.info("\n- PASS, PATCH command successfully completed \"%s\" request for chassis indicator LED" % args["set"])
     else:
@@ -71,12 +91,32 @@ def set_current_chassis_indicator_LED_state():
 if __name__ == "__main__":
     if args["script_examples"]:
         script_examples()
-    elif args["get"]:
+    if args["ip"] or args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+            verify_cert = False
+        check_supported_idrac_version()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    if args["get"]:
         get_current_chassis_indicator_LED_state()
     elif args["set"]:
         set_current_chassis_indicator_LED_state()
     else:
-        script_examples()
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
         
         
 
