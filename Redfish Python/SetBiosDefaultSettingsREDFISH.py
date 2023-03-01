d6246a18def38281a5b5d752267b571cbb2cf056
texroemer <30351677+texroemer@users.noreply.github.com>
2023-03-01 17:33:36 -0600

    Update SetBiosDefaultSettingsREDFISH.py

--- Redfish Python/SetBiosDefaultSettingsREDFISH.py
+++ Redfish Python/SetBiosDefaultSettingsREDFISH.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-#!/usr/bin/python3
-#
+
 # SetBiosDefaultSettingsREDFISH. Python script using Redfish API to set BIOS to default settings.
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
@@ -198,6 +196,3 @@ if __name__ == "__main__":
         reboot_server()
     else:
         logging.info("- INFO, argument --reboot not detected. Flag is still set to reset BIOS to default settings and this will get applied on next server manual reboot.")
-
-
-
