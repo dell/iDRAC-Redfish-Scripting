56c62b702883d8b42d257cda55b36ca1f18dc9d7
texroemer <30351677+texroemer@users.noreply.github.com>
2023-03-01 15:31:41 -0600

    Update ClearForeignConfigREDFISH.py

--- Redfish Python/ClearForeignConfigREDFISH.py
+++ Redfish Python/ClearForeignConfigREDFISH.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-#!/usr/bin/python3
-#
+
 # ClearForeignConfigREDFISH. Python script using Redfish API with OEM extension to clear a storage controller foreign configuration
 #
 # _author_ = Texas Roemer <Texas_Roemer@Dell.com>
@@ -64,7 +62,6 @@ def check_supported_idrac_version():
         logging.warning("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
         sys.exit(0)
 
-
 def get_storage_controllers():
     if args["x"]:
         response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/Storage' % idrac_ip,verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
@@ -77,8 +74,6 @@ def get_storage_controllers():
         controller_list.append(i['@odata.id'].split("/")[-1])
         print(i['@odata.id'].split("/")[-1])
     
-
-
 def get_pdisks_check_foreign_status():
     test_valid_controller_FQDD_string(args["get_disks"])
     if args["x"]:
@@ -217,10 +212,3 @@ if __name__ == "__main__":
     else:
         logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
         sys.exit(0)
-        
-    
-    
-        
-            
-        
-        
