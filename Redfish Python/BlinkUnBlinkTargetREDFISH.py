2e4743b5ab7d2dad58b07019283e38aac1a7fb19
texroemer <30351677+texroemer@users.noreply.github.com>
2023-03-01 15:22:36 -0600

    Update BlinkUnBlinkTargetREDFISH.py

--- Redfish Python/BlinkUnBlinkTargetREDFISH.py
+++ Redfish Python/BlinkUnBlinkTargetREDFISH.py
@@ -52,8 +52,6 @@ def script_examples():
     \n- BlinkUnBlinkTargetREDFISH.py -ip 192.168.0.120 -u root --unblink Disk.Virtual.0:RAID.Mezzanine.1-1, this example shows unblink a VD.""")
     sys.exit(0)
 
-    
-
 def check_supported_idrac_version():
     if args["x"]:
         response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellRaidService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
@@ -115,7 +113,6 @@ def get_pdisks():
       data = response.json()
       logging.info(" - Disk: %s, Raidstatus: %s" % (i, data['Oem']['Dell']['DellPhysicalDisk']['RaidStatus']))
     
-
 def get_virtual_disks():
     test_valid_controller_FQDD_string(args["get_virtualdisks"])
     if args["x"]:
@@ -165,9 +162,6 @@ def blink_unblink_device():
         logging.error("\n- POST command failure results:\n %s" % data)
         sys.exit(0)
     
-
-    
-
 if __name__ == "__main__":
     if args["script_examples"]:
         script_examples()
@@ -203,9 +197,3 @@ if __name__ == "__main__":
         blink_unblink_device()
     else:
         logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
-    
-    
-        
-            
-        
-        
