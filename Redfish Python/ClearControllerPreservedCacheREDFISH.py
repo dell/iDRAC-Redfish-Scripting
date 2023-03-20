2e068ec33635fa1cc464cf75b05f9aa27ed4dce4
texroemer <30351677+texroemer@users.noreply.github.com>
2023-03-20 16:23:59 -0500

    New script
    Python script using Redfish API with OEM extension to clear storage controller preserved cache.

---
+++ Redfish Python/ClearControllerPreservedCacheREDFISH.py
@@ -0,0 +1,200 @@
+#!/usr/bin/python3
+#
+# _author_ = Texas Roemer <Texas_Roemer@Dell.com>
+# _version_ = 1.0
+#
+# Copyright (c) 2023, Dell, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+import argparse
+import getpass
+import json
+import logging
+import re
+import requests
+import sys
+import time
+import warnings
+
+from datetime import datetime
+from pprint import pprint
+
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser(description="Python script using Redfish API with OEM extension to clear storage controller preserved cache.")
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in value \"true\" or \"false\". By default, this argument is not required and script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', help='Get executing script examples', action="store_true", dest="script_examples", required=False)
+parser.add_argument('--get-controllers', help='Get server storage controller FQDDs', action="store_true", dest="get_controllers", required=False)
+parser.add_argument('--get-controller-status', help='Get storage controller health status pass in storage controller FQDD. Example: \"RAID.Integrated.1-1\"', dest="get_controller_status", required=False)
+parser.add_argument('--clear', help='Clear storage controller preserved cache pass in storage controller FQDD.', required=False)
+args = vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
+
+def script_examples():
+    print("""\n- ClearControllerPreservedCacheREDFISH.py -ip 192.168.0.120 -u root -p calvin --get-controllers, this example will return storage controller FQDDs detected.
+    \n- ClearControllerPreservedCacheREDFISH.py -ip 192.168.0.120 -u root -p calvin --get-controller-status, this example will return storage controller health status.
+    \n- ClearControllerPreservedCacheREDFISH.py -ip 192.168.0.120 -x 7041fd6528bc5d9d88a34cdc14bf133a --clear RAID.Mezzanine.1-1, this example will clear controller RAID.Mezzanine.1-1 preserved cache using iDRAC x-auth token session.""")
+    sys.exit(0)
+    
+def check_supported_idrac_version():
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellRaidService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellRaidService' % idrac_ip, verify=verify_cert,auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned. Incorrect iDRAC username/password or invalid privilege detected." % response.status_code)
+        sys.exit(0)
+    elif response.status_code != 200:
+        logging.warning("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        sys.exit(0)
+
+def get_storage_controllers():
+    test_valid_controller_FQDD_string(args["get_storage_controllers"])
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/Storage' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
+    else:
+        response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/Storage' % idrac_ip, verify=verify_cert,auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code != 200:
+        logging.error("\n- WARNING, GET request failed status code %s returned" % response.status_code)
+        logging.error(data)
+        sys.exit(0)
+    logging.info("\n- Server controller(s) detected -\n")
+    controller_list = []
+    for i in data['Members']:
+        controller_list.append(i['@odata.id'].split("/")[-1])
+        print(i['@odata.id'].split("/")[-1])
+
+def get_controller_status():
+    test_valid_controller_FQDD_string(args["get_controller_status"])
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/Storage/%s?$select=Status' % (idrac_ip, args["get_controller_status"]), verify=verify_cert, headers={'X-Auth-Token': args["x"]})   
+    else:
+        response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/Storage/%s?$select=Status' % (idrac_ip, args["get_controller_status"]), verify=verify_cert,auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code != 200:
+        logging.error("\n- WARNING, GET request failed status code %s returned" % response.status_code)
+        logging.error(data)
+        sys.exit(0)
+    logging.info("\n- Current health status for controller %s\n" % args["get_controller_status"])
+    print(data["Status"])
+    
+def clear_controller_preserve_cache():
+    global job_id
+    method = "ClearControllerPreservedCache"
+    payload={"TargetFQDD": args["clear"]}
+    url = 'https://%s/redfish/v1/Dell/Systems/System.Embedded.1/DellRaidService/Actions/DellRaidService.ClearControllerPreservedCache' % (idrac_ip)
+    if args["x"]:
+        headers = {'content-type': 'application/json', 'X-Auth-Token': args["x"]}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert)
+    else:
+        headers = {'content-type': 'application/json'}
+        response = requests.post(url, data=json.dumps(payload), headers=headers, verify=verify_cert,auth=(idrac_username,idrac_password))
+    data = response.json()
+    if response.status_code == 202:
+        logging.info("\n- PASS: POST command passed to clear storage controller %s preserve cache, status code %s returned" % (args["clear"], response.status_code))
+        try:
+            job_id = response.headers['Location'].split("/")[-1]
+        except:
+            logging.error("- FAIL, unable to locate job ID in JSON headers output")
+            sys.exit(0)
+        logging.info("- Job ID %s successfully created for RAID method \"%s\"" % (job_id, method))
+    else:
+        logging.error("\n- FAIL, POST command failed to clear storage controller %s preserve cache, status code %s returned" % (args["clear"], response.status_code))
+        data = response.json()
+        logging.error("\n- POST command failure results:\n %s" % data)
+        sys.exit(0)
+
+def test_valid_controller_FQDD_string(x):
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/Storage/%s' % (idrac_ip, x),verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Systems/System.Embedded.1/Storage/%s' % (idrac_ip, x),verify=verify_cert,auth=(idrac_username, idrac_password))
+    if response.status_code != 200:
+        logging.error("\n- FAIL, either controller FQDD does not exist or typo in FQDD string name (FQDD controller string value is case sensitive)")
+        sys.exit(0)
+
+def loop_job_status():
+    start_time = datetime.now()
+    retry_count = 0
+    while True:
+        if retry_count == 10:
+            logging.error("- FAIL, GET job status retry count of 10 has been reached, script will exit")
+            sys.exit(0)
+        try:
+            if args["x"]:
+                response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Jobs/%s' % (idrac_ip, job_id), verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+            else:
+                response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Jobs/%s' % (idrac_ip, job_id), verify=verify_cert,auth=(idrac_username, idrac_password))
+        except requests.ConnectionError as error_message:
+            logging.error(error_message)
+            logging.error("\n- WARNING, GET request failed to check job status, retry again")
+            time.sleep(5)
+            retry_count += 1
+            continue
+        current_time = (datetime.now()-start_time)
+        if response.status_code != 200:
+            logging.error("\n- FAIL, GET command failed to check job status, return code is %s" % statusCode)
+            logging.error("Extended Info Message: {0}".format(req.json()))
+            sys.exit(0)
+        data = response.json()
+        if str(current_time)[0:7] >= "2:00:00":
+            logging.error("\n- FAIL: Timeout of 2 hours has been hit, script stopped\n")
+            sys.exit(0)
+        elif "Fail" in data['Message'] or "fail" in data['Message'] or data['JobState'] == "Failed":
+            logging.error("- FAIL: job ID %s failed, failed message is: %s" % (job_id, data['Message']))
+            sys.exit(0)
+        elif data['JobState'] == "Completed":
+            logging.info("\n--- PASS, Final Detailed Job Status Results ---\n")
+            for i in data.items():
+                if "odata" not in i[0] or "MessageArgs" not in i[0] or "TargetSettingsURI" not in i[0]:
+                    print("%s: %s" % (i[0],i[1]))
+            break
+        else:
+            logging.info("- INFO, job status not completed, current status: \"%s\"" % data['Message'].strip("."))
+            time.sleep(3)
+            
+if __name__ == "__main__":
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] or args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip = args["ip"]
+        idrac_username = args["u"]
+        if args["p"]:
+            idrac_password = args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+                verify_cert = False
+        check_supported_idrac_version()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    if args["get_controllers"]:
+        get_storage_controllers()
+    elif args["get_controller_status"]:
+        get_controller_status()
+    elif args["clear"]:
+        clear_controller_preserve_cache()
+        loop_job_status()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
