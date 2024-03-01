5f78d5383494c6ec5cab3319a3919d784038d25a
texroemer <30351677+texroemer@users.noreply.github.com>
2024-02-29 21:27:12 -0600

    New script to get iDRAC uptime
    New script to get current iDRAC uptime

---
+++ Redfish Python/GetIdracUpTimeREDFISH.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+#
+# _author_ = Texas Roemer <Texas_Roemer@Dell.com>
+# _version_ = 1.0
+#
+# Copyright (c) 2024, Dell, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+import argparse
+import getpass
+import json
+import logging
+import requests
+import sys
+import time
+import warnings
+
+from datetime import datetime
+from pprint import pprint
+
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser(description="Python script using Redfish API to get iDRAC current uptime")
+parser.add_argument('-ip',help='iDRAC IP address', required=False)
+parser.add_argument('-u', help='iDRAC username', required=False)
+parser.add_argument('-p', help='iDRAC password. If you do not pass in argument -p, script will prompt to enter user password which will not be echoed to the screen.', required=False)
+parser.add_argument('-x', help='Pass in X-Auth session token for executing Redfish calls. All Redfish calls will use X-Auth token instead of username/password', required=False)
+parser.add_argument('--ssl', help='SSL cert verification for all Redfish calls, pass in value \"true\" or \"false\". By default, this argument is not required and script ignores validating SSL cert for all Redfish calls.', required=False)
+parser.add_argument('--script-examples', help='Get executing script examples', action="store_true", dest="script_examples", required=False)
+
+args = vars(parser.parse_args())
+logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
+
+def script_examples():
+    print("""\n- GetIdracUpTimeREDFISH.py -ip 192.168.0.120 -u root -p calvin, this example will return current iDRAC uptime""")
+    sys.exit(0)
+
+def check_supported_idrac_version():
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code == 401:
+        logging.warning("\n- WARNING, status code %s returned. Incorrect iDRAC username/password or invalid privilege detected." % response.status_code)
+        sys.exit(1)
+    elif response.status_code != 200:
+        logging.warning("\n- WARNING, iDRAC version installed does not support this feature using Redfish API")
+        sys.exit(1)
+    
+def get_iDRAC_uptime():
+    logging.info("\n- INFO, getting current uptime for iDRAC %s" % idrac_ip)
+    if args["x"]:
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+    else:
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+    data = response.json()
+    if response.status_code != 200:
+        logging.error("\n- ERROR, GET request failed to get iDRAC uptime, error: \n%s" % data)
+        sys.exit(0)
+    current_date_time = data["DateTime"]
+    last_reset_time = data["LastResetTime"]
+    current_date_time_object = datetime.fromisoformat(current_date_time)
+    last_reset_time_object = datetime.fromisoformat(last_reset_time)
+    iDRAC_uptime = current_date_time_object - last_reset_time_object
+    days = iDRAC_uptime.days
+    hours = iDRAC_uptime.seconds // 3600
+    minutes = (iDRAC_uptime.seconds % 3600) // 60
+    seconds = iDRAC_uptime.seconds % 60
+    #print(f"Current iDRAC uptime: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
+    logging.info("\n- INFO, current iDRAC uptime: %s days, %s hours, %s minutes, %s seconds" % (days, hours, minutes, seconds))
+    
+        
+if __name__ == "__main__":
+    if args["script_examples"]:
+        script_examples()
+    if args["ip"] and args["ssl"] or args["u"] or args["p"] or args["x"]:
+        idrac_ip=args["ip"]
+        idrac_username=args["u"]
+        if args["p"]:
+            idrac_password=args["p"]
+        if not args["p"] and not args["x"] and args["u"]:
+            idrac_password = getpass.getpass("\n- Argument -p not detected, pass in iDRAC user %s password: " % args["u"])
+        if args["ssl"]:
+            if args["ssl"].lower() == "true":
+                verify_cert = True
+            elif args["ssl"].lower() == "false":
+                verify_cert = False
+            else:
+                verify_cert = False
+        else:
+            verify_cert = False
+        check_supported_idrac_version()
+    else:
+        logging.error("\n- FAIL, invalid argument values or not all required parameters passed in. See help text or argument --script-examples for more details.")
+        sys.exit(0)
+    get_iDRAC_uptime()
