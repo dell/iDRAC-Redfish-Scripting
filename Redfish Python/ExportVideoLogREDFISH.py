4d97f2c35145970eac9a8efbe6095af64c22544c
texroemer <30351677+texroemer@users.noreply.github.com>
2024-09-03 15:26:33 -0500

    Update ExportVideoLogREDFISH.py

--- Redfish Python/ExportVideoLogREDFISH.py
+++ Redfish Python/ExportVideoLogREDFISH.py
@@ -48,9 +48,9 @@ def script_examples():
 
 def check_supported_idrac_version():
     if args["x"]:
-        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellLCService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
     else:
-        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellLCService' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
     data = response.json()
     if response.status_code == 401:
         logging.warning("\n- WARNING, status code %s returned, check your iDRAC username/password is correct or iDRAC user has correct privileges to execute Redfish commands" % response.status_code)
@@ -60,7 +60,7 @@ def check_supported_idrac_version():
         sys.exit(0)
 
 def export_video_log():
-    url = 'https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService/Actions/DellLCService.ExportVideoLog' % (idrac_ip)
+    url = 'https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellLCService/Actions/DellLCService.ExportVideoLog' % (idrac_ip)
     method = "ExportVideoLog"
     logging.info("\n- INFO, collecting video capture logs, this may take 15-30 seconds to complete")
     payload={"ShareType":"Local"}
