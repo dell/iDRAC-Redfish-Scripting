6b5086953b1cccae1c121b26f2137628f02f68e4
texroemer <30351677+texroemer@users.noreply.github.com>
2024-09-03 14:43:16 -0500

    Update ExportFactoryConfigurationREDFISH.py

--- Redfish Python/ExportFactoryConfigurationREDFISH.py
+++ Redfish Python/ExportFactoryConfigurationREDFISH.py
@@ -57,9 +57,9 @@ def script_examples():
 
 def check_supported_idrac_version():
     if args["x"]:
-        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellLCService' % idrac_ip, verify=verify_cert, headers={'X-Auth-Token': args["x"]})
     else:
-        response = requests.get('https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
+        response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellLCService' % idrac_ip, verify=verify_cert, auth=(idrac_username, idrac_password))
     data = response.json()
     if response.status_code == 401:
         logging.warning("\n- WARNING, status code %s returned, check your iDRAC username/password is correct or iDRAC user has correct privileges to execute Redfish commands" % response.status_code)
@@ -70,7 +70,7 @@ def check_supported_idrac_version():
 
 def export_factory_configuration():
     global job_id
-    url = 'https://%s/redfish/v1/Dell/Managers/iDRAC.Embedded.1/DellLCService/Actions/DellLCService.ExportFactoryConfiguration' % (idrac_ip)
+    url = 'https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/DellLCService/Actions/DellLCService.ExportFactoryConfiguration' % (idrac_ip)
     method = "ExportFactoryConfiguration"
     payload={}
     if args["shareip"]:
