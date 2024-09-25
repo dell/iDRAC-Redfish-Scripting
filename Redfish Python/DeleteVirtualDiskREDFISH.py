43c5349e22a862b2d7401b0f52ea28a9eb84042a
texroemer <30351677+texroemer@users.noreply.github.com>
2024-09-25 15:55:13 -0500

    Update DeleteVirtualDiskREDFISH.py

--- Redfish Python/DeleteVirtualDiskREDFISH.py
+++ Redfish Python/DeleteVirtualDiskREDFISH.py
@@ -210,9 +210,9 @@ def delete_vd():
                 logging.error("- WARNING, retry count of 5 has been hit to determine job type. Manually check the job queue for %s job status" % job_id)
                 sys.exit(0)
             if args["x"]:
-                response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Jobs/%s' % (idrac_ip, job_id), verify=verify_cert, headers={'X-Auth-Token': args["x"]})
+                response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/Jobs/%s' % (idrac_ip, job_id), verify=verify_cert, headers={'X-Auth-Token': args["x"]})
             else:
-                response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Jobs/%s' % (idrac_ip, job_id), verify=verify_cert,auth=(idrac_username, idrac_password))
+                response = requests.get('https://%s/redfish/v1/Managers/iDRAC.Embedded.1/Oem/Dell/Jobs/%s' % (idrac_ip, job_id), verify=verify_cert,auth=(idrac_username, idrac_password))
             data = response.json()
             if "JobType" not in data.keys():
                 logging.error("- WARNING, unable to determine job type, retry in 10 seconds")
