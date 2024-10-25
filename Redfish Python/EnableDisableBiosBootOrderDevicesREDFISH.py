623c9fcfd3d190329c936fb870ee7eecb1bec9d9
texroemer <30351677+texroemer@users.noreply.github.com>
2024-10-25 14:10:44 -0500

    Update EnableDisableBiosBootOrderDevicesREDFISH.py

--- Redfish Python/EnableDisableBiosBootOrderDevicesREDFISH.py
+++ Redfish Python/EnableDisableBiosBootOrderDevicesREDFISH.py
@@ -263,8 +263,8 @@ def loop_job_status_final():
         if response.status_code == 200 or response.status_code == 202:
             logging.debug("- PASS, GET request passed to check job status")
         else:
-            logging.error("\n- FAIL, GET command failed to check job status, return code is %s" % statusCode)
-            logging.error("Extended Info Message: {0}".format(req.json()))
+            logging.error("\n- FAIL, GET command failed to check job status, return code is %s" % response.status_code)
+            logging.error("Extended Info Message: {0}".format(response.json()))
             sys.exit(0)
         data = response.json()
         if str(current_time)[0:7] >= "2:00:00":
